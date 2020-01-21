ddf95d0dd9ccafc85ea929b7c670cb3bbadfd1c8
anguss00 <angus.scheibner@utas.edu.au>
2020-01-21 16:36:44 +1100

    Add build script for webapp and api

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# builds the API and webapp
+
+WEBAPP_DIR=./nrmn-ui
+API_DIR=./api
+
+# set caching folder
+#yarn config set cache-folder /home/builder/.cache/yarn
+
+# install node packages
+yarn --cwd $WEBAPP_DIR install
+
+# build storybook
+yarn --cwd $WEBAPP_DIR ci-build-storybook
+
+# build the react app
+yarn --cwd $WEBAPP_DIR test-build
+
+# build the api
+mvn -f $API_DIR clean package
