343cf8452b190562a60f28559444d70ba8638023
Tanjona <tanjona.rafidison@utas.edu.au>
2020-08-27 11:37:02 +1000

    #2033: removing sql script

--- api/sql/REVINFO.sql
+++
@@ -1,9 +0,0 @@
-create table revinfo
-(
-    id             integer not null
-        constraint revinfo_pkey
-            primary key,
-    timestamp      bigint  not null,
-    api_request_id varchar(255),
-    username       varchar(255)
-);
