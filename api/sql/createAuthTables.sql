693f941fffe7862beb004b67534a10902871642a
pmbohm <pmbohm@utas.edu.au>
2020-08-27 14:48:41 +1000

    add nrmn schema to user_sec_roles

--- api/sql/createAuthTables.sql
+++ api/sql/createAuthTables.sql
@@ -3,13 +3,13 @@
 
     drop table if exists nrmn.user_sec_role cascade;
 
-    alter table if exists user_sec_roles 
+    alter table if exists nrmn.user_sec_roles
        drop constraint if exists FK_ROLE_USER_SEC;
 
-    alter table if exists user_sec_roles 
+    alter table if exists nrmn.user_sec_roles
        drop constraint if exists FK_USER_SEC_ROLE;
 
-    drop table if exists user_sec_roles cascade;
+    drop table if exists nrmn.user_sec_roles cascade;
 
     drop sequence if exists role_id_seq;
 
@@ -37,18 +37,18 @@
 create sequence role_id_seq start 1 increment 1;
 create sequence user_id_seq start 1 increment 1;
 
-    create table user_sec_roles (
+    create table nrmn.user_sec_roles (
        user_sec_id int4 not null,
         user_sec_role_id int8 not null,
         primary key (user_sec_id, user_sec_role_id)
     );
 
-    alter table if exists user_sec_roles 
+    alter table if exists nrmn.user_sec_roles
        add constraint FK_ROLE_USER_SEC 
        foreign key (user_sec_role_id) 
        references nrmn.user_sec_role;
 
-    alter table if exists user_sec_roles 
+    alter table if exists nrmn.user_sec_roles
        add constraint FK_USER_SEC_ROLE 
        foreign key (user_sec_id) 
        references nrmn.user_sec;
