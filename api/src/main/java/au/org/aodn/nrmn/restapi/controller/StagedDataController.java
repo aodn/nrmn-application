87773b924d0b5ef3994141fba50d28f7007eb4d0
Tanjona <tanjona.rafidison@utas.edu.au>
2020-10-07 14:12:49 +1100

    #2175: adding upload controller

---
+++ api/src/main/java/au/org/aodn/nrmn/restapi/controller/StagedDataController.java
@@ -0,0 +1,30 @@
+package au.org.aodn.nrmn.restapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/api/stage")
+public class StagedDataController {
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] files) {
+        try {
+            List<String> fileNames = new ArrayList<>();
+            Arrays.asList(files).stream().forEach(file -> {
+                //TOdo logic to valid and save file
+                fileNames.add(file.getOriginalFilename());
+            });
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).build();
+        }
+    }
+
+    }
