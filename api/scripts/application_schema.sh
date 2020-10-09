f5086a52529e451d98625defd7427de2b950ef47
Craig Jones <Craig.Jones@utas.edu.au>
2020-10-09 14:16:24 +1100

    Allow docker and local postgres port to use to be specified

--- api/scripts/application_schema.sh
+++ api/scripts/application_schema.sh
@@ -8,8 +8,39 @@
 # pg_format to be installed (apt install pgformatter)
 #
 
+# Get port to use for docker image if supplied
+
+usage() { echo "Usage: $0 [-d <docker port>] [-p <local postgres port> ] " 1>&2; exit 1; }
+
+while getopts "d:p:" OPTION; do
+    case "$OPTION" in
+        p)
+            POSTGRES_PORT=${OPTARG}
+            ;;
+        d)
+            DOCKER_PORT=${OPTARG}
+            ;;
+        ?)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Defaults
+
+if [[ -z ${POSTGRES_PORT} ]] ; then
+    POSTGRES_PORT=5432
+fi
+
+if [[ -z ${DOCKER_PORT} ]] ; then
+    DOCKER_PORT=5434
+fi
+
+# Determine script/project locations 
+
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-PROJECT_DIR="$(cd $SCRIPT_DIR/..; pwd -P)"
+PROJECT_DIR="$(cd ${SCRIPT_DIR}/..; pwd -P)"
 
 # Load docker postgres instance with migration schema
 
@@ -17,13 +48,13 @@ echo "Starting docker postgres instance..."
 
 export PGPASSWORD=postgres
 
-docker run -p 5434:5432 --name migration_db -e POSTGRES_PASSWORD=$PGPASSWORD -d postgis/postgis:9.6-2.5 > /dev/null
+docker run -p ${DOCKER_PORT}:5432 --name migration_db -e POSTGRES_PASSWORD=${PGPASSWORD} -d postgis/postgis:9.6-2.5 > /dev/null
 
 # wait for postgres to start listening on port 5434
 
 RETRIES=20
 
-until pg_isready -h localhost -U postgres -p 5434 > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until pg_isready -h localhost -U postgres -p ${DOCKER_PORT} > /dev/null 2>&1 || [[ ${RETRIES} -eq 0 ]]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
@@ -32,8 +63,8 @@ done
 
 echo "Loading migration schema..."
 
-psql -h localhost -U postgres -p 5434 -c "create database nrmn_migration template template_postgis" > /dev/null
-psql -h localhost -U postgres -p 5434 -d nrmn_migration -f "$PROJECT_DIR/src/main/resources/sql/migration.sql" > /dev/null 2>&1
+psql -h localhost -U postgres -p ${DOCKER_PORT} -c "create database nrmn_migration template template_postgis" > /dev/null
+psql -h localhost -U postgres -p ${DOCKER_PORT} -d nrmn_migration -f "$PROJECT_DIR/src/main/resources/sql/migration.sql" > /dev/null 2>&1
 
 # create a temporary directory for liquibase working files so no state is stored (changelog.xml/databasechangelog.csv)
  
@@ -47,12 +78,12 @@ liquibase --changeLogFile=changelog.xml \
           --classpath="$CHANGE_LOG_DIR" \
           --includeSchema=true \
           --driver=org.postgresql.Driver \
-          --url=jdbc:postgresql://localhost:5434/nrmn_migration \
+          --url=jdbc:postgresql://localhost:${DOCKER_PORT}/nrmn_migration \
           --username=postgres \
-          --password=$PGPASSWORD \
+          --password=${PGPASSWORD} \
           --defaultSchemaName=nrmn \
           diffChangeLog \
-          --referenceUrl=jdbc:postgresql://localhost:5432/nrmn_update \
+          --referenceUrl=jdbc:postgresql://localhost:${POSTGRES_PORT}/nrmn_update \
           --referenceUsername=postgres \
           --referencePassword=postgres \
           --referenceDefaultSchemaName=nrmn > /dev/null
