72a690e3f8100f9a3b6c4fec22bd3dd919705557
pmbohm <pmbohm@utas.edu.au>
2020-10-26 10:11:07 +1100

    Add loader icon

--- nrmn-ui/src/components/BaseForm.js
+++ nrmn-ui/src/components/BaseForm.js
@@ -5,25 +5,58 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Form from "@rjsf/material-ui"
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from "@material-ui/core/Button";
+import clsx from "clsx";
+import green from "@material-ui/core/colors/green";
+import {useSelector} from "react-redux";
+
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > * + *': {
-      marginTop: theme.spacing(2),
+      marginTop: theme.spacing(2)
     },
   },
+  rootFlex: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  wrapper: {
+    marginTop: theme.spacing(3),
+    position: 'relative',
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+  buttonProgress: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  }
 }));
-
-function getErrors(errors) {
-  return errors.map((  item, key) => {
-    return <div key={key}>{item}</div>
-  })
-}
 
 const BaseForm = (params) => {
 
   const classes = useStyles();
+
+  const loading = params.loading;
+
+  function submitForm(formData) {
+    params.onSubmit(formData);
+  }
+
+  function getErrors(errors) {
+    return errors.map((  item, key) => {
+      return <div key={key}>{item}</div>
+    })
+  }
 
   let errorAlert = params.errors ? <Alert  severity="error" variant="filled" >{getErrors(params.errors)}
   </Alert> : "";
@@ -41,11 +74,22 @@ const BaseForm = (params) => {
           <Form
               schema={params.schema}
               uiSchema={params.uiSchema}
-              onSubmit={params.onSubmit}
+              onSubmit={submitForm}
               gutterBottom={true}
               showErrorList={true}
-          />
-          {errorAlert}
+          >
+            {errorAlert}
+            <div className={classes.rootFlex}>
+              <div className={classes.wrapper}>
+                <Button type="submit"
+                        variant="contained"
+                        color="primary"
+                        disabled={loading}
+                >Submit</Button>
+                {loading && <CircularProgress size={20} className={classes.buttonProgress} />}
+              </div>
+            </div>
+          </Form>
         </Box>
       </Paper>
     </Grid>
