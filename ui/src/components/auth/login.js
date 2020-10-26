74bede956dd529485d3f25c5459a1ef0b674721d
pmbohm <pmbohm@utas.edu.au>
2020-10-27 09:55:57 +1100

    Rebase directory rename fix

--- ui/src/components/auth/login.js
+++ ui/src/components/auth/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import BaseForm from "../../../../nrmn-ui/src/components/BaseForm";
+import BaseForm from "../../../../ui/src/components/BaseForm";
 import {loginSubmitted} from "./auth-reducer";
 
 function Login()  {
