a8f5f4fc04000dac4eaa4f5c7676c011c7e5d5f0
pmbohm <pmbohm@utas.edu.au>
2020-11-05 17:24:35 +1100

    fix rebase problems

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/SecRoleRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/SecRoleRepository.java
@@ -5,9 +5,11 @@ import au.org.aodn.nrmn.restapi.model.db.SecUser;
 import au.org.aodn.nrmn.restapi.model.db.enums.SecRoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SecRoleRepository extends JpaRepository<SecRole, String>, JpaSpecificationExecutor<SecUser> {
     Optional<SecRole> findByName(SecRoleName name);
 }
