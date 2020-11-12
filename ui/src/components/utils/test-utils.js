8cb86ebcc290cf14ef9ae39612b00fd1ba152f5a
pmbohm <pmbohm@utas.edu.au>
2020-11-12 14:47:59 +1100

    more tests

--- ui/src/components/utils/test-utils.js
+++ ui/src/components/utils/test-utils.js
@@ -3,15 +3,29 @@ import { createMemoryHistory } from "history";
 import { Router, Route } from "react-router-dom";
 import { render } from "@testing-library/react";
 
+// USAGE
+// See https://testing-library.com/docs/dom-testing-library/api-queries
+// Returns the specified testing functions
+//
+//    const {findByTitle, findByText} = renderWithProviders(
+//         <Route path="/list/:entityName">
+//           <EntityList/>
+//         </Route>,
+//         {
+//           route: "/list/TestEntity"
+//         }
+//     );
+//     await findByTitle("Add new TestEntity");
+//
+//
+
 export function renderWithProviders(
     ui,
     {
       route = "/",
       history = createMemoryHistory({ initialEntries: [route] })
-    } = {},
-    apolloMocks
+    } = {}
 ) {
-  console.log("I am route inside renderWithProviders Wrapper:", route);
   return {
     ...render(
         <Router history={history}>
