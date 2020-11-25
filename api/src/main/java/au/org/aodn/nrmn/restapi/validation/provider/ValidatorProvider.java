0716d4a19fd264dc29a01c2896a943b11cac186e
Teej <tanjona.rafidison@utas.edu.au>
2020-11-26 09:58:38 +1100

    merge

--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/provider/ValidatorProvider.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/provider/ValidatorProvider.java
@@ -13,10 +13,6 @@ public interface ValidatorProvider<
         F extends BaseFormattedValidator,
         G extends  BaseGlobalValidator> {
   Seq<Tuple2<String, R>> getRowValidators();
-<<<<<<< HEAD
   Seq< F> getFormattedValidators();
-=======
-  Seq<F> getFormattedValidators();
->>>>>>> b4ac46e7b2a05e4737878ced287db8b00dd5a62e
   Seq<G> getGlobalValidators();
 }
