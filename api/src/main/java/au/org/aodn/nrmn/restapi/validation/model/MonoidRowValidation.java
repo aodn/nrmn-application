e64cb90f0b7b293b3e76b74335e8ed91ea3285df
Teej <tanjona.rafidison@utas.edu.au>
2020-12-03 10:30:32 +1100

    formatting

--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/model/MonoidRowValidation.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/model/MonoidRowValidation.java
@@ -8,13 +8,15 @@ import cyclops.function.Semigroup;
 
 public class MonoidRowValidation<T> implements Monoid<RowWithValidation<T>> {
 
-   private T defaultValue;
-   private Semigroup merger;
+    private T defaultValue;
+    private Semigroup merger;
+
     public MonoidRowValidation(T zero, Semigroup merger) {
         super();
         defaultValue = zero;
         this.merger = merger;
     }
+
     @Override
     public RowWithValidation<T> zero() {
         return new RowWithValidation(Seq.empty(), Validated.valid(defaultValue));
@@ -25,6 +27,6 @@ public class MonoidRowValidation<T> implements Monoid<RowWithValidation<T>> {
         return new RowWithValidation(
                 rowValid1.getRows().appendAll(rowValid2.getRows()),
                 rowValid1.valid.combine(merger, rowValid2.getValid())
-                );
+        );
     }
 }
