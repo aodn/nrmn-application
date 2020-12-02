4a3a98cc8a4b70eb8cdbba1512e9ad80d2798db7
Craig Jones <Craig.Jones@utas.edu.au>
2020-12-03 10:32:19 +1100

    Add projection for site containing embedded location (#91)

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/SiteRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/SiteRepository.java
@@ -1,29 +1,27 @@
 package au.org.aodn.nrmn.restapi.repository;
 
-import au.org.aodn.nrmn.restapi.model.db.AphiaRef;
 import au.org.aodn.nrmn.restapi.model.db.Site;
+import au.org.aodn.nrmn.restapi.model.db.projections.SiteList;
 import au.org.aodn.nrmn.restapi.repository.model.EntityCriteria;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
-
-
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = SiteList.class)
 @Tag(name = "sites")
 public interface SiteRepository extends JpaRepository<Site, Integer>, JpaSpecificationExecutor<Site>, EntityCriteria<Site> {
 
     @Override
     @Query("SELECT s FROM Site s WHERE s.siteCode = :code")
-    Optional<Site> findByCriteria(@Param("code")String siteCode);
+    Optional<Site> findByCriteria(@Param("code") String siteCode);
 
     @Override
     @RestResource
