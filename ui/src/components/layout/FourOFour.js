0434ac0976685e866759f9b164e6b42a355f56ba
Teej <tanjona.rafidison@utas.edu.au>
2021-01-15 15:38:51 +1100

    fof

---
+++ ui/src/components/layout/FourOFour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
+const FourOFour = () => {
+    return (<Box>
+        <Typography variant='h3'>The page you are looking for can’t be found.
+</Typography>
+    </Box>);
+};
+
+
+export default FourOFour;
