35610900745baebf4caf99e97eb971eadd8b4891
pmbohm <pmbohm@utas.edu.au>
2021-01-18 09:45:07 +1100

    Add Aggrid filter types other than text

--- ui/src/components/data-entities/EntityList.js
+++ ui/src/components/data-entities/EntityList.js
@@ -25,18 +25,29 @@ const cellRenderer = (params) => {
   return params.value;
 };
 
+const getCellFilter = (format) => {
+
+  const filterTypes = {
+    int64: 'agNumberColumnFilter',
+    'date-time': 'agDateColumnFilter'
+  };
+  return filterTypes[format] ? filterTypes[format] : 'agTextColumnFilter';
+
+};
+
 const schematoColDef = (schema, size, entityName) => {
 
   const fields = Object.keys(schema.properties);
   const widthSize = size.width / (fields.length + 1);
   const coldefs = fields.map(field => {
 
+    let type = schema.properties[field] ? schema.properties[field]?.format : 'string';
+
     return {
       field: field,
       width: widthSize,
       tooltipField: field,
-      // make every column use 'text' filter by default
-      filter: 'agTextColumnFilter',
+      filter: getCellFilter(type),
       cellRenderer: cellRenderer
     };
   });
@@ -182,8 +193,7 @@ const EntityList = () => {
                   tooltipShowDelay={0}
                   defaultColDef={{
                     sortable: true,
-                    resizable: true,// make every column use 'text' filter by default
-                    filter: 'agTextColumnFilter',
+                    resizable: true,
                     tooltipComponent: 'customTooltip',
                     floatingFilter: true,
                     headerComponentParams: {
