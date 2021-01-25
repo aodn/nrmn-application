1e9778cdf9f50589e320240a4072eadb14899bf3
Teej <tanjona.rafidison@utas.edu.au>
2021-01-25 15:45:31 +1100

    adding not found page

--- ui/src/components/layout/FourOFour.js
+++ ui/src/components/layout/FourOFour.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import { NavLink } from 'react-router-dom';
 
 const FourOFour = () => {
-    return (<Box>
-        <Typography variant='h3'>The page you are looking for can’t be found.
-</Typography>
-    </Box>);
+
+
+    const useStyles = makeStyles(() => ({
+        link: {
+            cursor: 'pointer'
+        }
+    }));
+    const classes = useStyles();
+
+    return (
+        <Grid container>
+            <Grid item md={12} lg={12} sm={12}>
+                <Box>
+                    <Typography align="center" style={{ fontSize: 125 }} variant='h1'>404</Typography>
+                    <Typography align="center" style={{ fontSize: 125 }} variant='h3'>Page not found.</Typography>
+                    <Typography align="center" style={{ fontSize: 20 }} variant='h5'>
+                        <Link  component={NavLink} className={classes.link} to="/list/StagedJob" color='secondary'> Jobs </Link> or
+                        <Link component={NavLink} className={classes.link} color='secondary' to="/home"> Home </Link>
+                    </Typography>
+
+                </Box>
+            </Grid>
+        </Grid>
+    );
 };
 
 
