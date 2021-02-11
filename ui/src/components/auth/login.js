0d09e29199361674315a6c720c24a2dc106a09a9
nspool <25042597+nspool@users.noreply.github.com>
2021-02-11 13:09:20 +1100

    decode jwt and store user roles in auth state

--- ui/src/components/auth/login.js
+++ ui/src/components/auth/login.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import BaseForm from '../../../../ui/src/components/BaseForm';
-import { loginSubmitted } from './auth-reducer';
-import { Redirect, useLocation } from 'react-router-dom';
+import {loginSubmitted} from './auth-reducer';
+import {Redirect, useLocation} from 'react-router-dom';
 
 const schema = {
-  'title': 'Login',
-  'type': 'object',
-  'required': [
-    'username',
-    'password'
-  ],
-  'properties': {
-    'username': {
-      'type': 'string',
-      'title': 'Email/Username',
-      'format': 'email'
+  title: 'Login',
+  type: 'object',
+  required: ['username', 'password'],
+  properties: {
+    username: {
+      type: 'string',
+      title: 'Email/Username',
+      format: 'email'
     },
-    'password': {
-      'type': 'string',
-      'title': 'Password'
+    password: {
+      type: 'string',
+      title: 'Password'
     }
   }
 };
@@ -31,12 +28,11 @@ const uiSchema = {
 };
 
 var Login = () => {
-
   const dispatch = useDispatch();
-  const errors = useSelector(state => state.auth.errors);
-  let loading = useSelector(state => state.auth.loading);
-  let success = useSelector(state => state.auth.success);
-  let redirect = useSelector(state => state.auth.redirect);
+  const errors = useSelector((state) => state.auth.errors);
+  let loading = useSelector((state) => state.auth.loading);
+  let success = useSelector((state) => state.auth.success);
+  let redirect = useSelector((state) => state.auth.redirect);
 
   const location = new URLSearchParams(useLocation().search).get('redirect');
 
@@ -47,17 +43,9 @@ var Login = () => {
     dispatch(loginSubmitted(form.formData));
   };
   if (success) {
-    return (<Redirect  component='link' to={redirect}></Redirect>);
+    return <Redirect component="link" to={redirect}></Redirect>;
   }
-  return (
-    <BaseForm
-      schema={schema}
-      uiSchema={uiSchema}
-      errors={errors}
-      loading={loading}
-      onSubmit={handleLogin}>
-    </BaseForm>
-  );
+  return <BaseForm schema={schema} uiSchema={uiSchema} errors={errors} loading={loading} onSubmit={handleLogin}></BaseForm>;
 };
 
 export default Login;
