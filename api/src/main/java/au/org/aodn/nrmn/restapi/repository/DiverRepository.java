0f40505195ebfba0368eb5ed3a7921751cba87f6
nspool <25042597+nspool@users.noreply.github.com>
2021-02-24 14:37:10 +1100

    diver api: add countQuery

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/DiverRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/DiverRepository.java
@@ -32,7 +32,9 @@ public interface DiverRepository
 
     @Override
     @RestResource
-    @Query(nativeQuery = true, value = "SELECT * FROM {h-schema}diver_ref d ORDER BY (CASE WHEN initials SIMILAR TO '%[a-zA-Z]' THEN 0 ELSE 1 END), d.initials")
+    @Query(value = "SELECT * FROM {h-schema}diver_ref d ORDER BY (CASE WHEN initials SIMILAR TO '%[a-zA-Z]' THEN 0 ELSE 1 END), d.initials", 
+           countQuery = "SELECT count(*) FROM {h-schema}diver_ref",
+           nativeQuery = true)
     Page<Diver> findAll(Pageable pageable);
 
     @Override
