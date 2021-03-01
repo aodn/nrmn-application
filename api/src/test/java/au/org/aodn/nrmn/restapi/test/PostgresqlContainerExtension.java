444c1225af1c21adb8a4f4a184326bd5386a9778
Craig Jones <Craig.Jones@utas.edu.au>
2021-03-01 12:11:35 +1100

    Need to confirm postgis image is test container compatible in recent versions

--- api/src/test/java/au/org/aodn/nrmn/restapi/test/PostgresqlContainerExtension.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/test/PostgresqlContainerExtension.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.extension.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 /* Adds a persistent postgres container for use in tests
- * Only started if running a test that references it and then only shut down 
+ * Only started if running a test that references it and then only shut down
  * when the jvm is shut down so it can be reused in multiple tests */
 
 public class PostgresqlContainerExtension implements Extension {
@@ -15,7 +16,10 @@ public class PostgresqlContainerExtension implements Extension {
 
     static {
         try {
-            PostgreSQLContainer container = new PostgreSQLContainer("mdillon/postgis:9.6");
+            DockerImageName postgisImage = DockerImageName
+                    .parse("mdillon/postgis:9.6")
+                    .asCompatibleSubstituteFor("postgres");
+            PostgreSQLContainer container = new PostgreSQLContainer(postgisImage);
             container.start();
             System.setProperty("DB_URL", container.getJdbcUrl());
             System.setProperty("DB_USERNAME", container.getUsername());
