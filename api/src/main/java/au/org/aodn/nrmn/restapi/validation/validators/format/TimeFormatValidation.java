83f8c48619e71a7f9f4657d6d2c6bbb656dd2396
Craig Jones <Craig.Jones@utas.edu.au>
2021-03-02 12:45:15 +1100

    Support requested time formats

--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/format/TimeFormatValidation.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/format/TimeFormatValidation.java
@@ -1,17 +1,17 @@
 package au.org.aodn.nrmn.restapi.validation.validators.format;
 
-import au.org.aodn.nrmn.restapi.model.db.StagedRowError;
 import au.org.aodn.nrmn.restapi.model.db.StagedRow;
-
+import au.org.aodn.nrmn.restapi.model.db.StagedRowError;
+import au.org.aodn.nrmn.restapi.util.TimeUtils;
 import cyclops.control.Validated;
 
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 
 public final class TimeFormatValidation extends BaseRowFormatValidation<LocalTime> {
+
     public TimeFormatValidation() {
-        super("Time", "H:mm");
+        super("Time", "Time");
     }
 
     @Override
@@ -19,7 +19,7 @@ public final class TimeFormatValidation extends BaseRowFormatValidation<LocalTim
         return validFormat(
                 StagedRow::getTime,
                 timeString -> {
-                    LocalTime localTime = LocalTime.parse(timeString, DateTimeFormatter.ofPattern(format));
+                    LocalTime localTime = TimeUtils.parseTime(timeString);
                     localTime.get(ChronoField.CLOCK_HOUR_OF_DAY);
                     localTime.get(ChronoField.MINUTE_OF_HOUR);
                     return Validated.valid(localTime);
