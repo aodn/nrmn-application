95d2265fee9ada2cb058161b0bf133185a7ad3c4
nspool <25042597+nspool@users.noreply.github.com>
2021-03-31 14:00:10 +1100

    additional survery controller tests

--- api/src/test/java/au/org/aodn/nrmn/restapi/controller/SurveyControllerIT.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/controller/SurveyControllerIT.java
@@ -9,6 +9,9 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import lombok.val;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +22,7 @@ import org.springframework.security.test.context.support.WithUserDetails;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(PostgresqlContainerExtension.class)
@@ -29,30 +33,40 @@ public class SurveyControllerIT {
 
     @Autowired
     private SurveyTestData surveyTestData;
+    private String NON_EXISTENT_SURVEY = "123123123";
 
     @Autowired
     private JwtToken jwtToken;
 
-    private RequestSpecification spec;
+    RequestSpecification allSurveysSpec, getSurveySpec;
 
     @BeforeEach
     public void setup() {
-        spec = new RequestSpecBuilder()
+        allSurveysSpec = new RequestSpecBuilder()
                 .setBaseUri(String.format("http://localhost:%s", port))
                 .setBasePath("/api/data/surveys")
                 .setContentType("application/json")
                 .addFilter(new ResponseLoggingFilter())
                 .addFilter(new RequestLoggingFilter())
                 .build();
+
+        getSurveySpec = new RequestSpecBuilder()
+            .setBaseUri(String.format("http://localhost:%s", port))
+            .setBasePath("/api/data/survey/")
+            .setContentType("application/json")
+            .addFilter(new ResponseLoggingFilter())
+            .addFilter(new RequestLoggingFilter())
+            .build();
     }
 
     @Test
     @WithUserDetails("test@gmail.com")
-    public void testGetSurvey() {
+    public void testGetSurveyList() {
+
         val testSurvey = surveyTestData.persistedSurvey();
 
         given()
-                .spec(spec)
+                .spec(allSurveysSpec)
                 .auth()
                 .oauth2(jwtToken.get())
                 .get()
@@ -62,4 +76,36 @@ public class SurveyControllerIT {
                 .body("surveyId", hasItems(testSurvey.getSurveyId()));
     }
 
+    @Test
+    @WithUserDetails("test@gmail.com")
+    public void testGetSurvey() {
+
+        val testSurvey = surveyTestData.persistedSurvey();
+
+        given().spec(getSurveySpec)
+               .auth()
+               .oauth2(jwtToken.get())
+               .get(testSurvey.getSurveyId().toString())
+               .then()
+               .assertThat()
+               .statusCode(200)
+               .body("surveyId", equalTo(testSurvey.getSurveyId()));
+    }
+
+    @Test
+    @WithUserDetails("test@gmail.com")
+    public void testNoSurvey() {
+
+        val testSurvey = surveyTestData.persistedSurvey();
+
+        assertFalse(testSurvey.getSurveyId().toString().equals(NON_EXISTENT_SURVEY));
+
+        given().spec(getSurveySpec)
+               .auth()
+               .oauth2(jwtToken.get())
+               .get(NON_EXISTENT_SURVEY)
+               .then()
+               .assertThat()
+               .statusCode(404);
+    }
 }
