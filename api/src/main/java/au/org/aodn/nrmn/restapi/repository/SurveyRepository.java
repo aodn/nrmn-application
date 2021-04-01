953e9c2fe57b6d5365ba8b62cc5a4d2ea429499c
Craig Jones <Craig.Jones@utas.edu.au>
2021-04-01 15:05:11 +1100

    aodn/backlog#2730 - duplicate survey check

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/SurveyRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/SurveyRepository.java
@@ -1,23 +1,34 @@
 package au.org.aodn.nrmn.restapi.repository;
 
+import au.org.aodn.nrmn.restapi.model.db.Site;
 import au.org.aodn.nrmn.restapi.model.db.Survey;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.QueryHint;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
+
+import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Integer>, JpaSpecificationExecutor<Survey> {
     @Query("SELECT t FROM #{#entityName} t WHERE t.id IN :ids")
     List<Survey> findByIdsIn(@Param("ids") List<Integer> ids);
+
+    @Query("SELECT s FROM #{#entityName} s " +
+            "WHERE s.site = :site " +
+            "  AND s.depth = :depth " +
+            "  AND s.surveyNum = :surveyNum " +
+            "  AND s.surveyDate = :date")
+    @QueryHints({@QueryHint(name = HINT_CACHEABLE, value = "true")})
+    List<Survey> findBySiteDepthSurveyNumDepth(
+            @Param("site") Site site,
+            @Param("depth") Integer depth,
+            @Param("surveyNum") Integer surveyNum,
+            @Param("date") Date date);
 }
