0f7938261d88d05e469e477aae8a19da2d3050f1
Craig Jones <Craig.Jones@utas.edu.au>
2021-04-13 15:50:55 +1000

    Align ATRC method checking with spec Improve readability of error message

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/StagedRowRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/StagedRowRepository.java
@@ -2,6 +2,7 @@ package au.org.aodn.nrmn.restapi.repository;
 
 import au.org.aodn.nrmn.restapi.model.db.StagedRow;
 import au.org.aodn.nrmn.restapi.repository.model.RowMethodBlock;
+import au.org.aodn.nrmn.restapi.repository.model.StagedSurveyMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -25,8 +26,11 @@ public interface StagedRowRepository extends JpaRepository<StagedRow, Long>, Jpa
     @Query("SELECT CONCAT(CONCAT(CONCAT(r.siteCode, r.date),r.depth), r.method) as id, r.method as method, r.block as block FROM StagedRow r  WHERE  r.stagedJob.id = :id AND r.method IN ('1','2') GROUP BY r.siteCode, r.date, r.depth, r.method, r.block")
     List<RowMethodBlock> findBlockMethods12(@Param("id") Long id);
 
-    @Query("SELECT CONCAT(CONCAT(r.siteCode, r.date),r.depth) as id, r.method as method FROM StagedRow r  WHERE  r.stagedJob.id = :id GROUP BY r.siteCode, r.date, r.depth, r.method ")
-    List<RowMethodBlock> getMethodsPerSurvey(@Param("id") Long  id);
+    @Query("SELECT r.siteCode as siteCode, r.date as date, r.depth as depth, r.method as method " +
+            "FROM StagedRow r  " +
+            "WHERE r.stagedJob.id = :id " +
+            "GROUP BY r.siteCode, r.date, r.depth, r.method ")
+    List<StagedSurveyMethod> getStagedSurveyMethods(@Param("id") Long id);
 
     @Query("delete from StagedRow r where r.id in (:ids)")
     void deleteAllByIds(@Param("ids") List<Long> ids);
--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/global/ATRCMethodCheck.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/global/ATRCMethodCheck.java
@@ -6,14 +6,16 @@ import au.org.aodn.nrmn.restapi.model.db.composedID.ErrorID;
 import au.org.aodn.nrmn.restapi.model.db.enums.ValidationCategory;
 import au.org.aodn.nrmn.restapi.model.db.enums.ValidationLevel;
 import au.org.aodn.nrmn.restapi.repository.StagedRowRepository;
-import au.org.aodn.nrmn.restapi.repository.model.RowMethodBlock;
+import au.org.aodn.nrmn.restapi.repository.model.StagedSurveyMethod;
 import au.org.aodn.nrmn.restapi.validation.BaseGlobalValidator;
 import cyclops.companion.Monoids;
 import cyclops.control.Validated;
+import cyclops.data.tuple.Tuple3;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @Component
@@ -22,38 +24,40 @@ public class ATRCMethodCheck extends BaseGlobalValidator {
     @Autowired
     StagedRowRepository stagedRowRepo;
 
-
     public ATRCMethodCheck() {
         super("ATRC Method check");
     }
 
     @Override
     public Validated<StagedRowError, String> valid(StagedJob job) {
-        val surveyGroupMethodCount = stagedRowRepo.getMethodsPerSurvey(job.getId());
-        val groupSurvey = surveyGroupMethodCount.stream().collect(Collectors.groupingBy(RowMethodBlock::getId));
-        return groupSurvey.entrySet().stream()
-                .map((entry) -> {
+        val stagedSurveyMethods = stagedRowRepo.getStagedSurveyMethods(job.getId());
+        val methodsGroupedBySurvey = stagedSurveyMethods.stream().collect(
+                Collectors.groupingBy(stagedSurveyMethod -> new Tuple3(stagedSurveyMethod.getSiteCode(),
+                        stagedSurveyMethod.getDate(), stagedSurveyMethod.getDepth())));
+        return methodsGroupedBySurvey.entrySet()
+                                     .stream()
+                                     .map((entry) -> {
 
-                    String methodStr = entry.getValue().stream()
-                            .map(RowMethodBlock::getMethod)
-                            .distinct()
-                            .sorted()
-                            .reduce("", (e1, e2) -> e1 + e2);
+                                         val methods = entry.getValue().stream()
+                                                            .map(StagedSurveyMethod::getMethod)
+                                                            .sorted()
+                                                            .collect(Collectors.toList());
 
-                    if (    methodStr.equals("12") ||
-                            methodStr.matches("[0345]*"))
-                        return Validated.<StagedRowError, String>valid("surveyGroup " + entry.getKey() + ": method valid");
-                    return Validated.<StagedRowError, String>invalid(new StagedRowError(
-                            new ErrorID(
-                                    null,
-                                    job.getId(),
-                                    entry.getKey() + "has incorrect set of methods:" + methodStr),
-                            ValidationCategory.GLOBAL,
-                            ValidationLevel.BLOCKING,
-                            ruleName,
-                            null
-                    ));
-                }).reduce(Validated.<StagedRowError, String>valid("survey methods check: nothing to validate"), (acc, validator) ->
+                                         if (methods.containsAll(Arrays.asList("1", "2"))
+                                          || methods.stream().anyMatch(s -> Arrays.asList("3", "4", "5").contains(s)))
+                                             return Validated.<StagedRowError, String>valid("surveyGroup " + entry.getKey() + ": method valid");
+
+                                         return Validated.<StagedRowError, String>invalid(new StagedRowError(
+                                                 new ErrorID(
+                                                         null,
+                                                         job.getId(),
+                                                         entry.getKey() + " has incorrect set of methods: " + methods),
+                                                 ValidationCategory.GLOBAL,
+                                                 ValidationLevel.WARNING,
+                                                 ruleName,
+                                                 null
+                                         ));
+                                     }).reduce(Validated.valid("survey methods check: nothing to validate"), (acc, validator) ->
                         acc.combine(Monoids.stringConcat, validator)
                 );
 
