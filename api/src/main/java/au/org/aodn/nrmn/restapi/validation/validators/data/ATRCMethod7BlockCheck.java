4ea2f324ec23bf73b74fcde3ee5671a0db30eb6e
Craig Jones <Craig.Jones@utas.edu.au>
2021-04-14 14:03:15 +1000

    Handle nulls

--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/data/ATRCMethod7BlockCheck.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/validators/data/ATRCMethod7BlockCheck.java
@@ -5,6 +5,8 @@ import au.org.aodn.nrmn.restapi.model.db.StagedRowError;
 import au.org.aodn.nrmn.restapi.validation.BaseRowValidator;
 import cyclops.control.Validated;
 
+import java.util.Optional;
+
 import static au.org.aodn.nrmn.restapi.model.db.enums.ValidationCategory.DATA;
 import static au.org.aodn.nrmn.restapi.model.db.enums.ValidationLevel.BLOCKING;
 
@@ -16,7 +18,12 @@ public class ATRCMethod7BlockCheck extends BaseRowValidator {
 
     @Override
     public Validated<StagedRowError, String> valid(StagedRow target) {
-        if (!target.getMethod().equals("7") || target.getBlock().equals("2")) {
+        Optional<String> method = Optional.ofNullable(target.getMethod());
+        Optional<String> block = Optional.ofNullable(target.getBlock());
+
+        if (!method.isPresent()
+                || !method.get().equals("7")
+                || (block.isPresent() && block.get().equals("2"))) {
             return Validated.valid("block is valid");
         } else {
             return getError(target, "Method 7 must be recorded on block 2", DATA, BLOCKING);
