c633d534ebfaa9daaa01a97e3e69ac99a6f8df2b
Craig Jones <Craig.Jones@utas.edu.au>
2021-04-15 12:01:18 +1000

    remove unused imports

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/SiteRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/SiteRepository.java
@@ -11,10 +11,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.persistence.QueryHint;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 
