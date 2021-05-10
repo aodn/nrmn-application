6b79813c65da31ffd7e6f05b6c36ad1830fb0fbf
Tanjona <67612124+utas-tajona@users.noreply.github.com>
2021-05-10 11:55:57 +1000

    rule + test (#273)
    * rule + test
    
    * updated with the right key
    
    * updating test
    
    * added to the provider
    
    * merge
    
    * merge

---
+++ api/src/test/java/au/org/aodn/nrmn/restapi/validation/validators/formatted/SpeciesBelongToMethodCheckTest.java
@@ -0,0 +1,40 @@
+package au.org.aodn.nrmn.restapi.validation.validators.formatted;
+
+import au.org.aodn.nrmn.restapi.model.db.ObservableItem;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.utils.ImmutableMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpeciesBelongToMethodCheckTest extends FormattedTestProvider{
+    @Test
+    public void matchingMethodShouldSuccess() {
+        val formatted = getDefaultFormatted().build();
+        formatted.setMethod(1);
+        formatted.setSpecies(
+                ObservableItem.builder()
+                        .obsItemAttribute(ImmutableMap.<String, String>builder()
+                                .put("is_M1", "true").build())
+                        .observableItemName("THE SPECIES").build());
+
+        val validator = new SpeciesBelongToMethodCheck();
+        val res =validator.valid(formatted);
+        assertTrue(res.isValid());
+    }
+
+    @Test
+    public void nonMatchingMethodShouldFail() {
+        val formatted = getDefaultFormatted().build();
+        formatted.setMethod(2);
+        formatted.setSpecies(
+                ObservableItem.builder()
+                        .obsItemAttribute(ImmutableMap.<String, String>builder()
+                                .put("is_M1", "true").build())
+                        .observableItemName("THE SPECIES").build());
+
+        val validator = new SpeciesBelongToMethodCheck();
+        val res =validator.valid(formatted);
+        assertTrue(res.isInvalid());
+    }
+}
