6391f378812d59423839ac7aff3bc9f03d02bbd3
Craig Jones <Craig.Jones@utas.edu.au>
2021-05-26 17:04:43 +1000

    Use spring caching instead of hibernate query cache - its much faster and safe for this repository

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/MeasureRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/MeasureRepository.java
@@ -1,6 +1,7 @@
 package au.org.aodn.nrmn.restapi.repository;
 
 import au.org.aodn.nrmn.restapi.model.db.Measure;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -17,7 +18,7 @@ import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 public interface MeasureRepository extends JpaRepository<Measure, Integer>, JpaSpecificationExecutor<Measure> {
 
     @Query("SELECT m FROM Measure m WHERE m.measureType.measureTypeId = :measureTypeId and m.seqNo = :seqNo")
-    @QueryHints({@QueryHint(name = HINT_CACHEABLE, value = "true")})
+    @Cacheable("measures")
     Optional<Measure> findByMeasureTypeIdAndSeqNo(@Param("measureTypeId") Integer measureTypeId,
                                                   @Param("seqNo") Integer seqNo);
 
