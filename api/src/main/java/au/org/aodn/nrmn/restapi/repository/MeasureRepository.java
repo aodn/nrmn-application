6fe24fdb4962c6e63fadfe53ffce8a66919632b1
Craig Jones <Craig.Jones@utas.edu.au>
2021-05-26 15:48:26 +1000

    Fix issue introduced in previous PR Update ingestion service unit tests to better test changes

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/MeasureRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/MeasureRepository.java
@@ -3,9 +3,22 @@ package au.org.aodn.nrmn.restapi.repository;
 import au.org.aodn.nrmn.restapi.model.db.Measure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.QueryHint;
+import java.util.Optional;
+
+import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
 
 @Repository
 public interface MeasureRepository extends JpaRepository<Measure, Integer>, JpaSpecificationExecutor<Measure> {
+
+    @Query("SELECT m FROM Measure m WHERE m.measureType.measureTypeId = :measureTypeId and m.seqNo = :seqNo")
+    @QueryHints({@QueryHint(name = HINT_CACHEABLE, value = "true")})
+    Optional<Measure> findByMeasureTypeIdAndSeqNo(@Param("measureTypeId") Integer measureTypeId,
+                                                  @Param("seqNo") Integer seqNo);
 
 }
