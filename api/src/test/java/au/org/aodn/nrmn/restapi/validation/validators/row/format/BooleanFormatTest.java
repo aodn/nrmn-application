79a40df895e4b9258990c042aa30bbd87e76be32
nspool <25042597+nspool@users.noreply.github.com>
2021-06-03 09:05:25 +1000

    validate 'use invert sizing' as yes or no

--- api/src/test/java/au/org/aodn/nrmn/restapi/validation/validators/row/format/BooleanFormatTest.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/validation/validators/row/format/BooleanFormatTest.java
@@ -9,15 +9,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BooleanFormatTest {
 
+    @Test
+    void stringNoisValid() {
+        val job = new StagedJob();
+        job.setReference("idJob");
+        val stage = new StagedRow();
+        stage.setIsInvertSizing("no");
+        stage.setStagedJob(job);
+        val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
+        assertTrue(res.isValid());
+        boolean validatedValue = res.fold(null,r->r);
+        assertFalse(validatedValue);
+    }
 
     @Test
-    void booleanShouldbeOk() {
+    void stringYesisValid() {
+        val job = new StagedJob();
+        job.setReference("idJob");
+        val stage = new StagedRow();
+        stage.setIsInvertSizing("yes");
+        stage.setStagedJob(job);
+        val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
+        assertTrue(res.isValid());
+        boolean validatedValue = res.fold(null,r->r);
+        assertTrue(validatedValue);
+    }
+
+    @Test
+    void stringTrueIsInvalid() {
         val job = new StagedJob();
         job.setReference("idJob");
         val stage = new StagedRow();
         stage.setIsInvertSizing("True");
         stage.setStagedJob(job);
         val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
-        assertTrue(res.isValid());
+        assertTrue(res.isInvalid());
+    }
+
+    @Test
+    void stringFalseIsInvalid() {
+        val job = new StagedJob();
+        job.setReference("idJob");
+        val stage = new StagedRow();
+        stage.setIsInvertSizing("false");
+        stage.setStagedJob(job);
+        val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
+        assertTrue(res.isInvalid());
+    }
+
+    @Test
+    void nullStringIsInvalid() {
+        val job = new StagedJob();
+        job.setReference("idJob");
+        val stage = new StagedRow();
+        stage.setIsInvertSizing(null);
+        stage.setStagedJob(job);
+        val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
+        assertTrue(res.isInvalid());
+    }
+
+    @Test
+    void emptyStringIsInvalid() {
+        val job = new StagedJob();
+        job.setReference("idJob");
+        val stage = new StagedRow();
+        stage.setIsInvertSizing("");
+        stage.setStagedJob(job);
+        val res = new BooleanFormatValidation(StagedRow::getIsInvertSizing, "sInvertSizing").valid(stage);
+        assertTrue(res.isInvalid());
     }
 }
