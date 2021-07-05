762816539999d7c8f4e112e3d92ab508afbd49dc
nspool <25042597+nspool@users.noreply.github.com>
2021-07-05 11:44:39 +1000

    fix postgres test container

--- api/src/test/java/au/org/aodn/nrmn/restapi/test/PostgresqlContainerExtension.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/test/PostgresqlContainerExtension.java
@@ -24,7 +24,6 @@ public class PostgresqlContainerExtension implements Extension {
             System.setProperty("DB_URL", container.getJdbcUrl());
             System.setProperty("DB_USERNAME", container.getUsername());
             System.setProperty("DB_PASSWORD", container.getPassword());
-            container.close();
         } catch (Throwable t) {
             logger.error("Couldn't start postgis container", t);
         }
