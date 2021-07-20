0818c696a6f531c5a4485375fd22a3c272f38d82
Alen Paulic <alen.paulic@utas.edu.au>
2021-07-21 08:45:27 +1000

    Remove pills/badges/tictacs

--- ui/src/components/job/JobView.js
+++ ui/src/components/job/JobView.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Chip, CircularProgress, Divider, Grid, Paper, Typography} from '@material-ui/core';
+import {Box, CircularProgress, Divider, Grid, Paper, Typography} from '@material-ui/core';
 import {useParams} from 'react-router';
-import AccountBalanceOutlinedIcon from '@material-ui/icons/AccountBalanceOutlined';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,7 +8,10 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import {makeStyles} from '@material-ui/core/styles';
-import {Link} from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Hidden from '@material-ui/core/Hidden';
 
 import {getFullJob} from '../../axios/api';
 
@@ -41,69 +43,76 @@ const JobView = () => {
       {job ? (
         <Grid container>
           <Grid item sm={12} md={12} lg={4}>
-            <Paper style={{padding: 15}} className={classes.paper}>
-              <Grid item lg={10} md={10}>
+            <Paper className={classes.paper}>
+              <Grid item lg={10} md={10} style={{padding: 15}}>
                 <Typography className={classes.title} variant="h5" color="primary">
                   {job.reference}
                 </Typography>
               </Grid>
-              <Grid container spacing={1} justify="flex-start">
-                <Grid item>
-                  <Chip
-                    size="small"
-                    avatar={<AccountBalanceOutlinedIcon />}
-                    label={job.program.programName}
-                    variant="outlined"
-                    mt={1}
-                  ></Chip>
-                </Grid>
-                <Grid item>
-                  <Chip size="small" label={job.source} variant="outlined"></Chip>
-                </Grid>
-                <Grid item>
-                  <Chip size="small" label={job.status} variant="outlined"></Chip>
-                </Grid>
 
-                {job.isExtendedSize && (
-                  <Grid item>
-                    <Chip size="small" color="secondary" label="Extended Size" variant="outlined"></Chip>
-                  </Grid>
-                )}
-              </Grid>
-              <Divider style={{marginTop: 15, marginBottom: 15}}></Divider>
-              <Grid>
-                <Grid item>
-                  <Typography>
-                    Created by: {job.creator.email} at {job.created}{' '}
-                  </Typography>
+              <Divider style={{margin: 15, marginTop: 0}}/>
+
+              <Grid container>
+                <Grid item xs={4} lg={6} style={{paddingBottom: 0}}>
+                  <List dense style={{paddingBottom: 0}}>
+                    <ListItem>
+                      <ListItemText primary="Program" secondary={job.program.programName} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary="Source" secondary={job.source} />
+                    </ListItem>
+                  </List>
                 </Grid>
-                {job.surveyIds &&
-                  job.surveyIds.length > 0 &&
-                  job.surveyIds.map((id) => (
-                    <Grid key={id} item>
-                      <Link to={`/data/survey/${id}`} variant="a">
-                        survey {id}
-                      </Link>
-                    </Grid>
-                  ))}
+                <Grid item xs={4} lg={6} style={{paddingBottom: 0}}>
+                  <List dense style={{paddingBottom: 0}}>
+                    <ListItem>
+                      <ListItemText primary="Status" secondary={job.status} />
+                    </ListItem>
+                    <ListItem>
+                      <ListItemText primary="Creator" secondary={job.creator.email} />
+                    </ListItem>
+                  </List>
+                </Grid>
+                <Grid display={{ lg: 'none' }} item xs={4} lg={6} style={{paddingTop: 0}}>
+                  <List dense style={{paddingTop: 0}}>
+                    <ListItem>
+                      <ListItemText primary="Date Created" secondary={new Date(job.created).toUTCString()} />
+                    </ListItem>
+                    <Hidden only="lg">
+                      <ListItem display={{ lg: 'none' }}>
+                        <ListItemText primary="Extended Size" secondary={job.isExtendedSize ? 'Yes' : 'No'} />
+                      </ListItem>
+                    </Hidden>
+                  </List>
+                </Grid>
+                <Hidden only={['xs', 'sm', 'md']}>
+                  <Grid item lg={6} style={{paddingTop: 0}}>
+                    <List dense style={{paddingTop: 0}}>
+                      <ListItem>
+                        <ListItemText primary="Extended Size" secondary={job.isExtendedSize ? 'Yes' : 'No'} />
+                      </ListItem>
+                    </List>
+                  </Grid>
+                </Hidden>
               </Grid>
+
             </Paper>
           </Grid>
           <Grid item sm={12} md={12} lg={7} className={classes.paper}>
             {job.logs && (
               <TableContainer component={Paper} style={{paddingRight: 15, paddingLeft: 15}}>
-                <Table className={classes.table} aria-label="Event Log Table">
+                <Table aria-label="Event Log Table">
                   <TableHead>
                     <TableRow>
                       <TableCell>Event Time</TableCell>
-                      <TableCell>Type</TableCell>
+                      <TableCell>Event</TableCell>
                       <TableCell>Details</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
                     {job.logs.map((log) => (
                       <TableRow key={log.id}>
-                        <TableCell component="th" scope="row">{log.eventTime}</TableCell>
+                        <TableCell component="th" scope="row">{new Date(log.eventTime).toUTCString()}</TableCell>
                         <TableCell>{log.eventType}</TableCell>
                         <TableCell>{log.details}</TableCell>
                       </TableRow>
