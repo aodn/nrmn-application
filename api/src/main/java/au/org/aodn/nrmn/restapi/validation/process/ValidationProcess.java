3a38476d7a2b7ff19ae2c2abfb90c7ad012eb263
nspool <25042597+nspool@users.noreply.github.com>
2021-08-10 14:12:20 +1000

    improve species and duplicate row validation
    species: skip if superseded  is empty
    duplicate rows: improve row hashing

--- api/src/main/java/au/org/aodn/nrmn/restapi/validation/process/ValidationProcess.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/validation/process/ValidationProcess.java
@@ -83,15 +83,11 @@ public class ValidationProcess {
     private static final double[] FISH_VALUES = { 2.5, 5, 7.5, 10, 12.5, 15, 20, 25, 30, 35, 40, 50, 62.5, 75, 87.5, 100, 112.5, 125, 137.5, 150, 162.5, 175, 187.5, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000 };
     private static final double[] INVERT_VALUES = { 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 16, 17, 18, 19, 20, 22, 24, 26, 28, 30 };
 
-    private String hashRow(StagedRow r) {
-        return r.getDepth() + r.getDiver() + r.getSiteCode() + r.getBlock() + r.getMethod() + r.getSpecies() + r.getDate() + r.getTime();
-    }
-
     // VALIDATION: Rows duplicated
     private Collection<ValidationRow> checkDuplicateRows(Collection<StagedRow> rows) {
         Map<String, List<Long>> mappedRows = new HashMap<String, List<Long>>();
         rows.stream().forEach(r -> {
-            String rowHash = hashRow(r);
+            String rowHash = r.getContentsHash();
             List<Long> rowIds = mappedRows.getOrDefault(rowHash, new ArrayList<Long>());
             rowIds.add(r.getId());
             mappedRows.put(rowHash, rowIds);
@@ -153,7 +149,7 @@ public class ValidationProcess {
                 Optional<ObservableItem> observableItem = species.stream().filter(s -> row.getSpecies().equalsIgnoreCase(s.getObservableItemName())).findAny();
                 if (observableItem.isPresent()) {
                     String supersededBy = observableItem.get().getSupersededBy();
-                    if (supersededBy != null)
+                    if (supersededBy != null && supersededBy.length() > 0)
                         errors.add(rowId, ValidationLevel.WARNING, "species", "Superseded by " + supersededBy);
                 } else {
                     errors.add(rowId, ValidationLevel.BLOCKING, "species", "Species does not exist");
