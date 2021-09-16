1d491501573570500ab3b53c09ca9701d4a05e86
Alex McKeown <a.mckeown@utas.edu.au>
2021-09-16 11:27:27 +1000

    Make versions get bumped via git tags

--- bumpversion.sh
+++ bumpversion.sh
@@ -4,57 +4,42 @@ set -eux
 
 RELEASE_BRANCH=master
 
-_get_maven_version() {
-  # extract version from pom.xml
-  version=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml)
+_get_tagged_version() {
+  # retrieve highest tag version
+  version=$(git tag -l '*.*.*' --sort=-version:refname | head -n 1)
   echo "${version}"
 }
 
-_set_maven_version() {
-  local suffix="$1"; shift
+_get_bumped_version() {
+  local version="$1"; shift
+  bump2version patch --current-version ${version} --allow-dirty --dry-run --list | grep -oP '^new_version=\K.*$'
+}
 
+_set_maven_version() {
+  local version="$1"; shift
   # use Maven versions plugin to bump version
-  mvn build-helper:parse-version versions:set \
-    -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}${suffix} \
-    versions:commit
+  mvn versions:set -DnewVersion="${version}" versions:commit
 }
 
 _update_git() {
   local version=$1; shift
-  git fetch --prune origin "+refs/tags/*:refs/tags/*"
   git add pom.xml '*/pom.xml'
   git commit -m "Bump version to ${version}"
   git tag -a -f -m "Bump version to ${version}" ${version}
-  git push --atomic origin "HEAD:${RELEASE_BRANCH}" tag "${version}"
-}
-
-_bumpversion() {
-  local suffix=$1; shift
-  _set_maven_version "$suffix" &>/dev/null
-  local new_version=$(_get_maven_version)
-  echo "${new_version}"
-}
-
-build() {
-  local suffix="-dev"
-  _bumpversion "$suffix"
+  git push origin tag "${version}"
 }
 
 release() {
-  local suffix=" "
-  _bumpversion "$suffix"
-  local new_version=$(_get_maven_version)
-  _update_git "${new_version}"
+ git fetch --prune origin "+refs/tags/*:refs/tags/*"
+ local OLD_VERSION=$(_get_tagged_version)
+ local NEW_VERSION=$(_get_bumped_version "${OLD_VERSION}")
+
+ _set_maven_version "${NEW_VERSION}"
+ _update_git "${NEW_VERSION}"
 }
 
 main() {
-  local mode=$1; shift
-
-  if [ "x${mode}" == "xrelease" ]; then
-    release
-  elif [ "x${mode}" == "xbuild" ]; then
-    build
-  fi
+  release
 
   exit 0
 }
