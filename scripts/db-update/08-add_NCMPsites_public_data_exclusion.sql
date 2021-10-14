e0390b238ba6b46be82bdabcda7332f9e44ad099
bpasquer <benedicte.pasquer@utas.edu.au>
2021-10-14 15:28:29 +1100

    truncate table+ query for Jurien site list update

--- scripts/db-update/08-add_NCMPsites_public_data_exclusion.sql
+++ scripts/db-update/08-add_NCMPsites_public_data_exclusion.sql
@@ -1,6 +1,7 @@
+TRUNCATE TABLE nrmn.public_data_exclusion;
 WITH stg1 AS (
 SELECT 2 AS program_id ,site_id FROM nrmn.site_ref
-WHERE site_code LIKE 'NCMP-S%'
-ORDER BY site_code)
+WHERE site_code LIKE 'NCMP-S%' or site_code LIKE 'JMP-S%'
+ORDER BY site_id)
 INSERT INTO nrmn.public_data_exclusion (program_id,site_id)
 SELECT * FROM stg1
