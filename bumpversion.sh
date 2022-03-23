44cc77cf510cffaf3bbb3ebddf9ba6e8e947210c
nspool <25042597+nspool@users.noreply.github.com>
2022-03-24 08:58:47 +1100

    Revert "bumpversion: add debugging"

--- bumpversion.sh
+++ bumpversion.sh
@@ -5,10 +5,7 @@ set -euxo pipefail
 main() {
   git fetch --prune origin "+refs/tags/*:refs/tags/*"
   OLD_VERSION=$(git tag -l '*.*.*' --sort=-version:refname | head -n 1)
-  BUMP_OUTPUT=$(bump2version --current-version $OLD_VERSION --list --tag --commit  --allow-dirty patch)
-  echo BUMP_OUTPUT: $BUMP_OUTPUT
-  NEW_VERSION=$(echo $BUMP_OUTPUT | grep -oP 'new_version=\K.*$')
-  echo NEW_VERSION: $NEW_VERSION
+  NEW_VERSION=$(bump2version --current-version $OLD_VERSION --list --tag --commit  --allow-dirty patch | grep -oP '^new_version=\K.*$')
   git push origin tag $NEW_VERSION
 
   exit 0
