d0464b54f5ff4454c45999963bcec3a56317b11a
nspool <25042597+nspool@users.noreply.github.com>
2022-03-25 08:24:31 +1100

    site api improvements
     change /sites to /site
     remove @Schema annotations that are no longer needed
     remove verbose logging
     suppress warning in yarn build step

--- api/src/main/java/au/org/aodn/nrmn/restapi/dto/site/SiteListItem.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/dto/site/SiteListItem.java
@@ -1,7 +1,6 @@
 package au.org.aodn.nrmn.restapi.dto.site;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,30 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SiteListItem {
-    @Schema(hidden = true)
     Integer siteId;
-
-    @Schema(title = "Site Code")
     String siteCode;
-
-    @Schema(title = "Site Name")
     String siteName;
-
-    @Schema(title = "Location")
     String locationName;
-
-    @Schema(title = "State")
     String state;
-
-    @Schema(title = "Country")
     String country;
-
-    @Schema(title = "Latitude")
     Float latitude;
-
-    @Schema(title = "Longitude")
     Float longitude;
-
-    @Schema(title = "Is Active")
     Boolean isActive;
 }
