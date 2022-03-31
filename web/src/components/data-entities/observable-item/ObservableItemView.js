33c96882ee0c271011754f2e55810663422d89cb
nspool <25042597+nspool@users.noreply.github.com>
2022-04-01 08:30:48 +1100

    obsitem view: add back formatting

--- web/src/components/data-entities/observable-item/ObservableItemView.js
+++ web/src/components/data-entities/observable-item/ObservableItemView.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Divider, Grid, Typography, Paper, Button} from '@mui/material';
+import {Box, Divider, Grid, Typography, Button} from '@mui/material';
 import {getEntity} from '../../../api/api';
 import {useParams} from 'react-router';
 import {Edit} from '@mui/icons-material';
 import {NavLink} from 'react-router-dom';
+import EntityContainer from '../../containers/EntityContainer';
+import CustomTextInput from '../../input/CustomTextInput';
 
 const ObservableItemView = () => {
   const id = useParams()?.id;
@@ -17,106 +19,102 @@ const ObservableItemView = () => {
   }, [id]);
 
   return (
-    <Grid container alignItems="center" justifyContent="center" style={{minHeight: '70vh'}}>
-      <Paper>
-        <Box m={2} display="flex" flexDirection="row">
-          <Box p={1} flexGrow={1}>
-            <Typography variant="h4">Observable Item Details</Typography>
-          </Box>
-          <Box>
-            <Button style={{width: '100%'}} component={NavLink} to={`/reference/observableItem/${id}/edit`} startIcon={<Edit>edit</Edit>}>
-              Edit
-            </Button>
-          </Box>
+    <EntityContainer name="Sites" goBackTo="/reference/observableItems">
+      <Box m={2} display="flex" flexDirection="row" width="100%">
+        <Box flexGrow={1}>
+          <Typography variant="h4">Observable Items</Typography>
         </Box>
-        <Box p={2}>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <b>ID:</b> {data.observableItemId}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Species Name:</b> {data.observableItemName}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Observable Item Type:</b> {data.obsItemTypeName}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Common Name</b> {data.commonName}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Aphia ID:</b> {data.aphiaId ?? '---'}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Aphia Relation:</b> {data.aphiaRelTypeName ?? '---'}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Superseded By:</b> {data.supersededBy ?? '---'}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Superseded Names:</b> {data.supersededNames ?? '---'}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Superseded IDs:</b> {data.supersededIds ?? '---'}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Letter Code:</b> {data.letterCode ?? '---'}
-            </Grid>
+        <Box>
+          <Button variant="outlined" component={NavLink} to={`/reference/observableItem/${id}/edit`} startIcon={<Edit>edit</Edit>}>
+            Edit
+          </Button>
+        </Box>
+      </Box>
+      <Box p={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="ID" formData={data.observableItemId} />
           </Grid>
-        </Box>
-        <Divider />
-        <Box p={2}>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <b>Phylum:</b> {data.phylum}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Class:</b> {data.class}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Order:</b> {data.order}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Family:</b> {data.family}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Genus:</b> {data.genus}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Report Group:</b> {data.reportGroup}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Habitat Group:</b> {data.habitatGroups}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Species Epithet:</b> {data.speciesEpithet}
-            </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Species Name" formData={data.observableItemName} />
           </Grid>
-        </Box>
-        <Divider />
-        <Box p={2}>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <b>Length-Weight a:</b> {data.lengthWeightA}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Length-Weight b:</b> {data.lengthWeightB}
-            </Grid>
-            <Grid item xs={6}>
-              <b>Length-Weight cf:</b> {data.lengthWeightCf}
-            </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Observable Item Type" formData={data.obsItemTypeName} />
           </Grid>
-        </Box>
-        <Box p={2}>
-          {data.obsItemAttribute ? (
-            data.obsItemAttribute
-          ) : (
-            <Typography variant="subtitle2" component="i">
-              No Other Attributes
-            </Typography>
-          )}
-        </Box>
-      </Paper>
-    </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Common Name" formData={data.commonName} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Aphia ID" formData={data.aphiaId ?? '---'} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Aphia Relation" formData={data.aphiaRelTypeName ?? '---'} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Superseded By" formData={data.supersededBy ?? '---'} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Superseded Names" formData={data.supersededNames ?? '---'} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Superseded IDs" formData={data.supersededIds ?? '---'} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Letter Code" formData={data.letterCode ?? '---'} />
+          </Grid>
+        </Grid>
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Phylum" formData={data.phylum} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Class" formData={data.class} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Order" formData={data.order} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Family" formData={data.family} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Genus" formData={data.genus} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Report Group" formData={data.reportGroup} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Habitat Group" formData={data.habitatGroups} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Species Epithet" formData={data.speciesEpithet} />
+          </Grid>
+
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Length-Weight a" formData={data.lengthWeightA} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Length-Weight b" formData={data.lengthWeightB} />
+          </Grid>
+          <Grid item xs={6}>
+            <CustomTextInput readOnlyInput label="Length-Weight cf" formData={data.lengthWeightCf} />
+          </Grid>
+          <Grid item xs={12}>
+            <Box p={2} pl={8}>
+              {data.obsItemAttribute ? (
+                data.obsItemAttribute
+              ) : (
+                <Typography variant="subtitle2" component="i">
+                  No Other Attributes
+                </Typography>
+              )}
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </EntityContainer>
   );
 };
 
