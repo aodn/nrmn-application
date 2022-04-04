76586247f75d97f839633e2db15f8c6d75b703ae
nspool <25042597+nspool@users.noreply.github.com>
2022-04-05 09:08:17 +1000

    survey location: distance validation
    Warn only if survey location is greater than 10m from site.

--- api/src/main/java/au/org/aodn/nrmn/restapi/util/SpacialUtil.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/util/SpacialUtil.java
@@ -2,15 +2,14 @@ package au.org.aodn.nrmn.restapi.util;
 
 public class SpacialUtil {
 
-    public static double getDistance(double lat1, double lon1, double lat2, double lon2) {
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        } else {
-            double theta = lon1 - lon2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-            // 60 * 1.1515 * 1.609344;
-            return Math.toDegrees(Math.acos(dist)) * 111.18957696;
-        }
+    // Haversine formula. See: http://www.movable-type.co.uk/scripts/latlong.html
+    public static double getDistanceLatLongMeters(double lat1, double lon1, double lat2, double lon2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLon = Math.toRadians(lon2-lon1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double dist = earthRadius * c;
+        return dist;
     }
-
 }
