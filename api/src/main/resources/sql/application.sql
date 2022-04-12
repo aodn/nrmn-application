ed7140eeabbc64d6549150b58be0e19708368b59
Viet Nguyen <vietnguyengit@outlook.com>
2022-04-12 11:50:37 +1000

    Create index if not exists & Drop materialized view if exists

--- api/src/main/resources/sql/application.sql
+++ api/src/main/resources/sql/application.sql
@@ -371,5 +371,5 @@ CREATE SEQUENCE IF NOT EXISTS nrmn.user_id_seq;
 
 ALTER SEQUENCE nrmn.staged_row_id_seq INCREMENT BY 100;
 
-CREATE UNIQUE INDEX idx_unique_ep_rarity_frequency_taxon ON nrmn.ep_rarity_frequency(taxon);
-CREATE UNIQUE INDEX idx_unique_ep_m1 ON nrmn.ep_m1(survey_id, recorded_species_name, size_class, block, "method", diver);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_ep_rarity_frequency_taxon ON nrmn.ep_rarity_frequency(taxon);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_ep_m1 ON nrmn.ep_m1(survey_id, recorded_species_name, size_class, block, "method", diver);
