040ca7ba0ff5dc3a9708eb981282d6a60a736514
utas-raymondng <manfai.ng@utas.edu.au>
2022-05-11 14:16:49 +1000

    [3943] Autosize works for SiteList.js

--- web/src/components/data-entities/diver/DiverList.js
+++ web/src/components/data-entities/diver/DiverList.js
@@ -9,112 +9,115 @@ import {Add, Save} from '@mui/icons-material';
 import 'ag-grid-enterprise';
 
 const DiverList = () => {
-  const [delta, setDelta] = useState([]);
-  const [errors, setErrors] = useState([]);
-  const dGridRef = useRef(null);
+    const [delta, setDelta] = useState([]);
+    const [errors, setErrors] = useState([]);
+    const dGridRef = useRef(null);
 
-  // Auto size function to be call each time data changed, so the grid always autofit
-  const autoSizeAll = (skipHeader) => {
-      if(dGridRef.current != null) {
-          dGridRef.current.columnApi.autoSizeAllColumns(skipHeader);
-      }};
+    // Auto size function to be call each time data changed, so the grid always autofit
+    const autoSizeAll = (skipHeader) => {
+        if (dGridRef.current != null) {
+            dGridRef.current.columnApi.autoSizeAllColumns(skipHeader);
+        }
+    };
 
-  const onGridReady = (event) => {
-    async function fetchDivers(event) {
-      await getResult('divers').then(
-          (res) => {
-              // Use setRowData in api will not trigger onGridReady but onDataChange event.
-              // if you use useState and connect row to setRowData then you will
-              // keep fire onGridReady as row change
-              event.api.setRowData(res.data);
-          });
-    }
-    fetchDivers(event).then(() => {
-        // Now we have the data to do auto sizing, however the build in function only auto size visible rows,
-        // so when user scroll we need to auto size again
-        autoSizeAll(false);
-    });
-  };
+    const onGridReady = (event) => {
+        async function fetchDivers(event) {
+            await getResult('divers').then(
+                (res) => {
+                    // Use setRowData in api will not trigger onGridReady but onDataChange event.
+                    // if you use useState and connect row to setRowData then you will
+                    // keep fire onGridReady as row change
+                    event.api.setRowData(res.data);
+                });
+        }
 
-  // Why need this extra redraw?
-  // useEffect(() => {
-  //   if (gridApi) gridApi.redrawRows();
-  // }, [gridApi, delta, errors]);
+        fetchDivers(event).then(() => {
+            // Now we have the data to do auto sizing, however the build in function only auto size visible rows,
+            // so when user scroll we need to auto size again
+            autoSizeAll(false);
+        });
+    };
 
-  const onCellValueChanged = (e) => {
-    setDelta((data) => {
-      const newData = {...data};
-      newData[e.data.diverId] = e.data;
-      autoSizeAll(false);
-      return newData;
-    });
-  };
+    // Why need this extra redraw?
+    // useEffect(() => {
+    //   if (gridApi) gridApi.redrawRows();
+    // }, [gridApi, delta, errors]);
 
-  const chooseCellStyle = (params) => {
-    if (params.context.errors.some((e) => e.id === params.data.diverId)) return {backgroundColor: red[100]};
-    if (params.context.delta[params.data.diverId]) return {backgroundColor: grey[100]};
-    return null;
-  };
+    const onCellValueChanged = (e) => {
+        setDelta((data) => {
+            const newData = {...data};
+            newData[e.data.diverId] = e.data;
+            autoSizeAll(false);
+            return newData;
+        });
+    };
 
-  const tooltipValueGetter = (params) => {
-    const error = params.context.errors.find((e) => e.id === params.data.diverId);
-    if (error) return error.message;
-  };
+    const chooseCellStyle = (params) => {
+        if (params.context.errors.some((e) => e.id === params.data.diverId)) return {backgroundColor: red[100]};
+        if (params.context.delta[params.data.diverId]) return {backgroundColor: grey[100]};
+        return null;
+    };
 
-  const saveGrid = () => {
-    setErrors([]);
-    entityEdit('divers', Object.values(delta)).then((res) => {
-      res.status === 400 ? setErrors(res.data) : setDelta({});
-    });
-  };
+    const tooltipValueGetter = (params) => {
+        const error = params.context.errors.find((e) => e.id === params.data.diverId);
+        if (error) return error.message;
+    };
 
-  return (
-    <>
-      <Box display="flex" flexDirection="row" p={1} pb={1}>
-        <Box flexGrow={1}>
-          <Typography variant="h4">Divers</Typography>
-        </Box>
-        <Box>
-          <Button variant="contained" to="/reference/diver" component={NavLink} startIcon={<Add />}>
-            New Diver
-          </Button>
-        </Box>
-        <Box mx={2}>
-          <Button variant="contained" startIcon={<Save />} onClick={saveGrid} disabled={Object.keys(delta).length < 1}>
-            Save Changes
-          </Button>
-        </Box>
-      </Box>
-      <AgGridReact
-        ref={dGridRef}
-        className="ag-theme-material"
-        getRowId={(r) => r.data.diverId}
-        rowHeight={20}
-        fillHandleDirection="y"
-        pagination={true}
-        enableBrowserTooltips
-        onCellValueChanged={onCellValueChanged}
-        context={{useOverlay: 'Loading Divers', delta, errors}}
-        components={{loadingOverlay: LoadingOverlay}}
-        loadingOverlayComponent="loadingOverlay"
-        onGridReady={onGridReady}
-        onBodyScroll={ (event) => autoSizeAll(false)}
-        defaultColDef={{
-          editable: true,
-          sortable: true,
-          resizable: true,
-          suppressMenu: true,
-          floatingFilter: true,
-          filter: 'agTextColumnFilter',
-          cellStyle: chooseCellStyle,
-          tooltipValueGetter: tooltipValueGetter
-        }}
-      >
-        <AgGridColumn field="initials" />
-        <AgGridColumn field="fullName" />
-      </AgGridReact>
-    </>
-  );
+    const saveGrid = () => {
+        setErrors([]);
+        entityEdit('divers', Object.values(delta)).then((res) => {
+            res.status === 400 ? setErrors(res.data) : setDelta({});
+        });
+    };
+
+    return (
+        <>
+            <Box display="flex" flexDirection="row" p={1} pb={1}>
+                <Box flexGrow={1}>
+                    <Typography variant="h4">Divers</Typography>
+                </Box>
+                <Box>
+                    <Button variant="contained" to="/reference/diver" component={NavLink} startIcon={<Add/>}>
+                        New Diver
+                    </Button>
+                </Box>
+                <Box mx={2}>
+                    <Button variant="contained" startIcon={<Save/>} onClick={saveGrid}
+                            disabled={Object.keys(delta).length < 1}>
+                        Save Changes
+                    </Button>
+                </Box>
+            </Box>
+            <AgGridReact
+                ref={dGridRef}
+                className="ag-theme-material"
+                getRowId={(r) => r.data.diverId}
+                rowHeight={20}
+                fillHandleDirection="y"
+                pagination={true}
+                enableBrowserTooltips
+                onCellValueChanged={onCellValueChanged}
+                context={{useOverlay: 'Loading Divers', delta, errors}}
+                components={{loadingOverlay: LoadingOverlay}}
+                loadingOverlayComponent="loadingOverlay"
+                onGridReady={onGridReady}
+                onBodyScroll={(event) => autoSizeAll(false)}
+                defaultColDef={{
+                    editable: true,
+                    sortable: true,
+                    resizable: true,
+                    suppressMenu: true,
+                    floatingFilter: true,
+                    filter: 'agTextColumnFilter',
+                    cellStyle: chooseCellStyle,
+                    tooltipValueGetter: tooltipValueGetter
+                }}
+            >
+                <AgGridColumn field="initials"/>
+                <AgGridColumn field="fullName"/>
+            </AgGridReact>
+        </>
+    );
 };
 
 export default DiverList;
