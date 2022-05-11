ba41923590dd76af843c0dc25175c7479c16c19a
utas-raymondng <manfai.ng@utas.edu.au>
2022-05-11 14:16:49 +1000

    [3943] Autosize works for SiteList.js

--- web/src/components/data-entities/site/SiteList.js
+++ web/src/components/data-entities/site/SiteList.js
@@ -10,164 +10,178 @@ import {entityDelete} from '../../../api/api';
 import 'ag-grid-enterprise';
 
 const SiteList = () => {
-  const [rowData, setRowData] = useState();
-  const [redirect, setRedirect] = useState();
-  const [dialogState, setDialogState] = useState({open: false});
-  const sGridRef = useRef(null);
+    const [redirect, setRedirect] = useState();
+    const [dialogState, setDialogState] = useState({open: false});
+    const sGridRef = useRef(null);
 
     // Auto size function to be call each time data changed, so the grid always autofit
     const autoSizeAll = (skipHeader) => {
-        if(sGridRef.current != null) {
+        if (sGridRef.current != null) {
             sGridRef.current.columnApi.autoSizeAllColumns(skipHeader);
-        }};
+        }
+    };
 
-  const onGridReady = useCallback(() => {
-    async function fetchSites() {
-      await getResult('sites').then(
-          (res) => {
-              setRowData(res.data);
-              autoSizeAll(false);
-          });
-    }
-    fetchSites();
-  }, []);
+    const onGridReady = (event) => {
+        async function fetchSites(event) {
+            await getResult('sites').then(
+                (res) => {
+                    // Use setRowData in api will not trigger onGridReady but onDataChange event.
+                    // if you use useState and connect row to setRowData then you will
+                    // keep fire onGridReady as row change
+                    event.api.setRowData(res.data);
+                });
+        }
+
+        fetchSites(event).then(() => {
+            // Now we have the data to do auto sizing, however the build in function only auto size visible rows,
+            // so when user scroll we need to auto size again
+            autoSizeAll(false);
+        });
+    };
 
   if (redirect) return <Navigate to={`/reference/site/${redirect}`} />;
 
-  const dialog = (
-    <Dialog disableBackdropClick disableEscapeKeyDown maxWidth="xs" open>
-      <DialogTitle>Delete Site?</DialogTitle>
-      <DialogContent>
-        Are you sure you want to permanently delete this site?
-        <Box p={2}>
-          <Typography variant="subtitle2">{dialogState.description}</Typography>
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button variant="outlined" autoFocus onClick={() => setDialogState({open: false})}>
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          onClick={(e) => {
-            entityDelete('site', dialogState.item.siteId).then(
-                (i) => {e.api.applyTransaction({remove: [dialogState.item]});
-              setDialogState({open: false});
-            });
-          }}
-        >
-          Delete Site
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+    const dialog = (
+        <Dialog disableBackdropClick disableEscapeKeyDown maxWidth="xs" open>
+            <DialogTitle>Delete Site?</DialogTitle>
+            <DialogContent>
+                Are you sure you want to permanently delete this site?
+                <Box p={2}>
+                    <Typography variant="subtitle2">{dialogState.description}</Typography>
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="outlined" autoFocus onClick={() => setDialogState({open: false})}>
+                    Cancel
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={(e) => {
+                        entityDelete('site', dialogState.item.siteId).then(
+                            (i) => {
+                                sGridRef.current.api.applyTransaction({remove: [dialogState.item]});
+                                setDialogState({open: false});
+                            });
+                    }}
+                >
+                    Delete Site
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
 
-  return (
-    <>
-      {dialogState.open && dialog}
-      <Box display="flex" flexDirection="row" pt={1}>
-        <Box p={1} pl={2} flexGrow={1}>
-          <Typography variant="h4">Sites</Typography>
-        </Box>
-        <Box mr={4}>
-          <Button style={{width: '100%'}} to="/reference/site" component={NavLink} variant={'contained'} startIcon={<Add></Add>}>
-            New Site
-          </Button>
-        </Box>
-      </Box>
-      <Box flexGrow={1} overflow="hidden" className="ag-theme-material">
-        <AgGridReact
-          ref={sGridRef}
-          rowHeight={24}
-          pagination={true}
-          rowData={rowData}
-          enableCellTextSelection={true}
-          onGridReady={onGridReady()}
-          context={{useOverlay: 'Loading Sites'}}
-          components={{loadingOverlay: LoadingOverlay}}
-          loadingOverlayComponent="loadingOverlay"
-          suppressCellFocus={true}
-          defaultColDef={{sortable: true, resizable: true, filter: 'agTextColumnFilter', floatingFilter: true}}
-        >
-          <AgGridColumn
-            width={40}
-            field="siteId"
-            headerName=""
-            suppressMovable={true}
-            suppressMenu={true}
-            filter={false}
-            resizable={false}
-            sortable={false}
-            cellRenderer={() => <Edit/> }
-            cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
-            onCellClicked={(e) => {
-              if (e.event.ctrlKey) {
-                window.open(`/reference/site/${e.data.siteId}/edit`, '_blank').focus();
-              } else {
-                setRedirect(`${e.data.siteId}/edit`);
-              }
-            }}
-          />
-          <AgGridColumn
-            width={100}
-            field="siteCode"
-            cellStyle={{cursor: 'pointer'}}
-            onCellClicked={(e) => {
-              if (e.event.ctrlKey) {
-                window.open(`/reference/site/${e.data.siteId}`, '_blank').focus();
-              } else {
-                setRedirect(`${e.data.siteId}`);
-              }
-            }}
-          />
-          <AgGridColumn field="siteName" />
-          <AgGridColumn field="locationName" />
-          <AgGridColumn field="state" />
-          <AgGridColumn field="country" />
-          <AgGridColumn field="latitude" />
-          <AgGridColumn field="longitude" />
-          <AgGridColumn suppressMenu={true} field="isActive" headerName="Active" />
-          <AgGridColumn
-            width={40}
-            field="siteId"
-            headerName=""
-            suppressMovable={true}
-            suppressMenu={true}
-            filter={false}
-            resizable={false}
-            sortable={false}
-            cellRenderer={() => <CopyAll/>}
-            cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
-            onCellClicked={(e) => {
-              if (e.event.ctrlKey) {
-                window.open(`/reference/site/${e.data.siteId}/clone`, '_blank').focus();
-              } else {
-                setRedirect(`${e.data.siteId}/clone`);
-              }
-            }}
-          />
-          <AgGridColumn
-            width={60}
-            field="observableItemId"
-            headerName=""
-            suppressMovable={true}
-            suppressMenu={true}
-            filter={false}
-            resizable={false}
-            sortable={false}
-            cellRenderer={(e) => e.data.isActive ? <></> : <Delete/>}
-            cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
-            onCellClicked={(e) => {
-              setDialogState({
-                open: true,
-                item: e.data
-              });
-            }}
-          />
-        </AgGridReact>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            {dialogState.open && dialog}
+            <Box display="flex" flexDirection="row" pt={1}>
+                <Box p={1} pl={2} flexGrow={1}>
+                    <Typography variant="h4">Sites</Typography>
+                </Box>
+                <Box mr={4}>
+                    <Button style={{width: '100%'}} to="/reference/site" component={NavLink} variant={'contained'}
+                            startIcon={<Add></Add>}>
+                        New Site
+                    </Button>
+                </Box>
+            </Box>
+            <Box flexGrow={1} overflow="hidden" className="ag-theme-material">
+                <AgGridReact
+                    ref={sGridRef}
+                    rowHeight={24}
+                    pagination={true}
+                    enableCellTextSelection={true}
+                    onGridReady={onGridReady}
+                    onBodyScroll={(event) => autoSizeAll(false)}
+                    context={{useOverlay: 'Loading Sites'}}
+                    components={{loadingOverlay: LoadingOverlay}}
+                    loadingOverlayComponent="loadingOverlay"
+                    suppressCellFocus={true}
+                    defaultColDef={{
+                        sortable: true,
+                        resizable: true,
+                        filter: 'agTextColumnFilter',
+                        floatingFilter: true
+                    }}
+                >
+                    <AgGridColumn
+                        width={40}
+                        field="siteId"
+                        headerName=""
+                        suppressMovable={true}
+                        suppressMenu={true}
+                        filter={false}
+                        resizable={false}
+                        sortable={false}
+                        cellRenderer={() => <Edit/> }
+                        cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
+                        onCellClicked={(e) => {
+                            if (e.event.ctrlKey) {
+                                window.open(`/reference/site/${e.data.siteId}/edit`, '_blank').focus();
+                            } else {
+                                setRedirect(`${e.data.siteId}/edit`);
+                            }
+                        }}
+                    />
+                    <AgGridColumn
+                        width={100}
+                        field="siteCode"
+                        cellStyle={{cursor: 'pointer'}}
+                        onCellClicked={(e) => {
+                            if (e.event.ctrlKey) {
+                                window.open(`/reference/site/${e.data.siteId}`, '_blank').focus();
+                            } else {
+                                setRedirect(`${e.data.siteId}`);
+                            }
+                        }}
+                    />
+                    <AgGridColumn field="siteName"/>
+                    <AgGridColumn field="locationName"/>
+                    <AgGridColumn field="state"/>
+                    <AgGridColumn field="country"/>
+                    <AgGridColumn field="latitude"/>
+                    <AgGridColumn field="longitude"/>
+                    <AgGridColumn suppressMenu={true} field="isActive" headerName="Active"/>
+                    <AgGridColumn
+                        width={40}
+                        field="siteId"
+                        headerName=""
+                        suppressMovable={true}
+                        suppressMenu={true}
+                        filter={false}
+                        resizable={false}
+                        sortable={false}
+                        cellRenderer={() => <CopyAll/>}
+                        cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
+                        onCellClicked={(e) => {
+                            if (e.event.ctrlKey) {
+                                window.open(`/reference/site/${e.data.siteId}/clone`, '_blank').focus();
+                            } else {
+                                setRedirect(`${e.data.siteId}/clone`);
+                            }
+                        }}
+                    />
+                    <AgGridColumn
+                        width={60}
+                        field="observableItemId"
+                        headerName=""
+                        suppressMovable={true}
+                        suppressMenu={true}
+                        filter={false}
+                        resizable={false}
+                        sortable={false}
+                        cellRenderer={(e) => e.data.isActive ? <></> : <Delete/>}
+                        cellStyle={{paddingLeft: '10px', color: 'grey', cursor: 'pointer'}}
+                        onCellClicked={(e) => {
+                            setDialogState({
+                                open: true,
+                                item: e.data
+                            });
+                        }}
+                    />
+                </AgGridReact>
+            </Box>
+        </>
+    );
 };
 
 export default SiteList;
