0a937bd336fd782ac8ad335e9e20694f1fcf4b90
nspool <25042597+nspool@users.noreply.github.com>
2022-05-12 15:52:58 +1000

    Update bumpversion.sh

--- bumpversion.sh
+++ bumpversion.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 main() {
   git fetch --prune origin "+refs/tags/*:refs/tags/*"
+  git fetch --tags
   OLD_VERSION=$(git describe --match "*.*.*" --tags --abbrev=0)
   NEW_VERSION=$(bump2version --current-version $OLD_VERSION --list --tag --commit --verbose --allow-dirty patch | grep -oP 'new_version=\K.*$')
   git push origin tag $NEW_VERSION
