b3d0aa2d756034b29ae44d01f47349bf21ae01fa
utas-raymondng <manfai.ng@utas.edu.au>
2022-05-27 18:24:25 +1000

    Add test for filter change to SiteList.js

---
+++ web/src/components/data-entities/site/__test__/SiteList.filter.test.tsx
@@ -0,0 +1,122 @@
+// @ts-ignore
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { describe, beforeAll, afterEach } from '@jest/globals';
+import { Router } from 'react-router-dom';
+import * as axiosInstance from '../../../../api/api';
+import { AxiosResponse } from 'axios';
+import SiteList from '../SiteList';
+import { createMemoryHistory } from 'history';
+import stateFilterHandler from '../../../../common/state-event-handler/StateFilterHandler';
+
+describe('<SiteList/> filter testing', () => {
+  let mockGetEntity;
+  let mockGetFiltersForId;
+  const columns = ['Site Code','Site Name','Location Name','State','Country','Latitude','Longitude','Active'];
+
+  beforeAll(() => {
+    mockGetEntity = jest.spyOn(axiosInstance, 'getResult');
+    mockGetFiltersForId = jest.spyOn(stateFilterHandler, 'getFiltersForId');
+
+    // silence errors caused by not setting an AG Grid licence
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    mockGetEntity.mockReset();
+    mockGetFiltersForId.mockReset();
+  });
+
+  test('Render necessary fields and no filter restored', async () => {
+    const canned = require('./sample1.json');
+
+    // Override function so that it return the data we set.
+    mockGetEntity.mockImplementation((url) => {
+
+      const raw = {
+        config: undefined,
+        data: canned,
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        status: 200,
+        statusText: url
+      };
+
+      return new Promise<AxiosResponse>((resolve => {
+        resolve(raw);
+      }));
+    });
+
+    const history = createMemoryHistory({initialEntries:[{state: {resetFilters: true}}]});
+    const {rerender} = render(<Router location={history.location} navigator={history}><SiteList/></Router>);
+
+    // Data loaded due to mock object being called once
+    await waitFor(() => expect(mockGetEntity).toHaveBeenCalledTimes(1), {timeout: 10000})
+      .then(() => {
+        // verify default columns exist
+        columns.forEach(x => {
+          expect(screen.queryAllByText(x).length).toBeGreaterThanOrEqual(1);
+        });
+      })
+      .finally(() => {
+        // Refresh the dom tree
+        rerender(<Router location={history.location} navigator={history}><SiteList/></Router>);
+
+        expect(screen.getByText('Lhohaesf')).toBeInTheDocument();
+        expect(screen.getByText('Silesv')).toBeInTheDocument();
+        expect(screen.getByText('Janeng 2')).toBeInTheDocument();
+
+        // Restore filter not called if you pass the resetFilter false to the component
+        expect(mockGetFiltersForId).toBeCalledTimes(0);
+      });
+  });
+
+  test('Render necessary fields with filter restored', async () => {
+    const canned = require('./sample1.json');
+
+    // Filter set will cause some items disappeared
+    mockGetFiltersForId.mockImplementation((id) => {
+      return '{"siteName":{"filterType":"text","type":"contains","filter":"Channel"}}';
+    });
+
+    // Override function so that it return the data we set.
+    mockGetEntity.mockImplementation((url) => {
+
+      const raw = {
+        config: undefined,
+        data: canned,
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        status: 200,
+        statusText: url
+      };
+
+      return new Promise<AxiosResponse>((resolve => {
+        resolve(raw);
+      }));
+    });
+
+    const history = createMemoryHistory({initialEntries:[{state: {resetFilters: false}}]});
+    const {rerender} = render(<Router location={history.location} navigator={history}><SiteList/></Router>);
+
+    // Data loaded due to mock object being called once
+    await waitFor(() => expect(mockGetEntity).toHaveBeenCalledTimes(1), {timeout: 10000})
+      .then(() => {
+        // verify default columns exist
+        columns.forEach(x => {
+          expect(screen.queryAllByText(x).length).toBeGreaterThanOrEqual(1);
+        });
+      })
+      .finally(() => {
+        // Refresh the dom tree
+        rerender(<Router location={history.location} navigator={history}><SiteList/></Router>);
+
+        // Restore filter called
+        expect(mockGetFiltersForId).toBeCalledTimes(1);
+
+        expect(screen.getByText('Channel')).toBeInTheDocument();
+        screen.findByText('Lhohaesf').then(i => expect(i).toBe({}));
+        screen.findByText('Janeng 2').then(i => expect(i).toBe({}));
+      });
+  });
+});
