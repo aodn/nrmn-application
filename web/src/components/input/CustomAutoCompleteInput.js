dc6ebb8c8fb0604c8d72796a72366f36a1acaec1
utas-raymondng <manfai.ng@utas.edu.au>
2022-06-01 18:06:35 +1000

    changed color to orange if text not inside options

--- web/src/components/input/CustomAutoCompleteInput.js
+++ web/src/components/input/CustomAutoCompleteInput.js
@@ -1,21 +1,89 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import {CircularProgress, TextField, Typography} from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import {PropTypes} from 'prop-types';
+import { makeStyles } from '@mui/styles';
 
 const CustomAutoCompleteInput = ({label, field, options, onChange, formData, errors}) => {
-  const error = errors?.find((f) => f.property === field);
+
+  const ERROR_TYPE = {NORMAL: 0, WARNING: 1, ERROR: 2};
+
+  const useStyles = value =>
+    makeStyles(theme => ({
+      root: {
+        // Text in the text box color
+        '& .MuiOutlinedInput-root.Mui-error': {
+          color: acquireValidationColor(value.type),
+          '& fieldset': {
+            borderColor: acquireValidationColor(value.type),
+          },
+        },
+        // Helper text color
+        '& .MuiFormHelperText-root.Mui-error' :{
+          color: acquireValidationColor(value.type)
+        },
+      },
+    }));
+
+  const acquireValidationColor = state => {
+    switch (state) {
+      case ERROR_TYPE.WARNING:
+        return 'DarkOrange';
+      case ERROR_TYPE.ERROR:
+        return 'red';
+      default:
+        return 'black';
+    }
+  };
+
+  const errorReducer = (state, action) => {
+    state.display = action.internalError !== undefined? action.internalError : state.display;
+
+    if(state.display) {
+      state.message = state.internalErrorMessage;
+    }
+    else {
+      state.message = '';
+    }
+    return state;
+  };
+
+  const [validate, setValidate] = useReducer(errorReducer, {
+    internalErrorMessage: 'New "' + label + '" will be created',
+    display: false,
+    type: ERROR_TYPE.WARNING,
+    message: ''
+  });
+
+  const onInputChange = (event, value, reason) => {
+    if(options.find(p => p.toLowerCase() === value.toLowerCase()) === undefined && value !== '') {
+      setValidate({ internalError: true});
+    }
+    else {
+      setValidate({ internalError: false});
+    }
+  };
+
+  const classes = useStyles(validate)();
 
   return options ? (
     <>
       <Typography variant="subtitle2">{label}</Typography>
       <Autocomplete
+        className={classes.root}
         options={options}
         freeSolo
         value={formData}
         onBlur={(e) => onChange(e.target.value)}
+        onInputChange={(e, v, r) => onInputChange(e,v,r)}
         renderInput={(params) => (
-          <TextField {...params} size="small" color="primary" variant="outlined" error={error} helperText={error?.message} />
+          <TextField
+            {...params}
+            size="small"
+            color="primary"
+            variant="outlined"
+            error={validate.display}
+            helperText={validate.message}/>
         )}
       />
     </>
