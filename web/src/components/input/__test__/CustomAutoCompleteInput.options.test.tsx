d7d5246414a7c23ceb95b1f99dad1f603907d0c0
utas-raymondng <manfai.ng@utas.edu.au>
2022-06-02 12:00:28 +1000

    add test to cover CustomAutoCompleteInput change

--- web/src/components/input/__test__/CustomAutoCompleteInput.options.test.tsx
+++ web/src/components/input/__test__/CustomAutoCompleteInput.options.test.tsx
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { describe, beforeAll, afterEach } from '@jest/globals';
+import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
 import * as axiosInstance from '../../../api/api';
 import { AxiosResponse } from 'axios';
@@ -12,8 +13,63 @@ import { createMemoryHistory } from 'history';
 
 jest.setTimeout(10000);
 
-describe('<CustomAutoCompleteInput/> filter testing', () => {
-  test('Text not appears in options create warning', async () => {
+describe('<CustomAutoCompleteInput/> options behavior', () => {
 
+  let onChangeFunction;
+
+  const cannedOptions = ['item1', 'item2', 'item3'];
+
+  beforeAll(() => {
+    onChangeFunction = jest.fn();
   });
+
+  afterEach(() => {
+    onChangeFunction.mockReset();
+  });
+
+  test('Input appears in options, no warning created', async () => {
+    let i;
+    onChangeFunction.mockImplementation((t) => i = t);
+
+    const {rerender} = render(<CustomAutoCompleteInput
+      label={'test label'}
+      options={cannedOptions}
+      formData={null}
+      field={'testField'}
+      errors={null}
+      onChange={onChangeFunction}
+    />);
+
+    userEvent.type(screen.getByRole('combobox'), 'item1');
+    expect(screen.queryByText('New "test label" will be created')).toBeNull();
+
+    // Pretended you move away from the text box by clicking the label hence onBlur trigger
+    userEvent.click(screen.getByText('test label'));
+    expect(onChangeFunction).toBeCalledTimes(1);
+    expect(i === 'item1').toBeTruthy();
+  });
+
+  test('Input do not appears in options, created warning', async () => {
+    let i;
+    onChangeFunction.mockImplementation((t) => i = t);
+
+    const {rerender} = render(<CustomAutoCompleteInput
+      label={'test label'}
+      options={cannedOptions}
+      formData={null}
+      field={'testField'}
+      errors={null}
+      onChange={onChangeFunction}
+    />);
+
+    userEvent.type(screen.getByRole('combobox'), 'item10');
+    expect(screen.getByText('New "test label" will be created').textContent)
+      .toEqual('New "test label" will be created');
+
+    // Pretended you move away from the text box by clicking the label hence onBlur trigger
+    userEvent.click(screen.getByText('test label'));
+    expect(onChangeFunction).toBeCalledTimes(1);
+    expect(i === 'item10').toBeTruthy();
+  });
+
 });
