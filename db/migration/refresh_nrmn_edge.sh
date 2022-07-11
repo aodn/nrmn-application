fc27014d8e0375b7169c89b4264821c675d27c0e
utas-raymondng <manfai.ng@utas.edu.au>
2022-07-11 10:46:07 +1000

    change branch from master to main

--- db/migration/refresh_nrmn_edge.sh
+++ db/migration/refresh_nrmn_edge.sh
@@ -9,8 +9,8 @@ pg_restore --no-privileges --no-owner --role=nrmn_edge -v -d nrmn_edge nrmn_migr
 
 # Apply application.sql and test_user.sql
 
-wget -O application.sql 'https://raw.githubusercontent.com/aodn/nrmn-application/master/api/src/main/resources/sql/application.sql'
-wget -O test_user.sql 'https://raw.githubusercontent.com/aodn/nrmn-application/master/api/src/test/resources/testdata/TEST_USER.sql'
+wget -O application.sql 'https://raw.githubusercontent.com/aodn/nrmn-application/main/api/src/main/resources/sql/application.sql'
+wget -O test_user.sql 'https://raw.githubusercontent.com/aodn/nrmn-application/main/api/src/test/resources/testdata/TEST_USER.sql'
 
 psql -c 'set role nrmn_edge'  -f application.sql -f test_user.sql nrmn_edge
 
