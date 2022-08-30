74ea1ad1a7a27c1ddf64312b6a1d41100bb3995b
utas-raymondng <manfai.ng@utas.edu.au>
2022-08-30 16:29:42 +1000

    Remove un-use code

--- api/src/main/java/au/org/aodn/nrmn/restapi/repository/LocationRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/repository/LocationRepository.java
@@ -21,17 +21,4 @@ public interface LocationRepository extends JpaRepository<Location, Integer>, Jp
     @Override
     @RestResource
     Optional<Location> findById(Integer integer);
-
-//    @Query(value = "SELECT distinct loc.location_id as id, loc.location_name as locationName, " +
-//        "CASE loc.is_active WHEN true THEN 'Active' ELSE 'Inactive' END as status, " +
-//        "string_agg(DISTINCT sit.country, ', ' ORDER BY sit.country) AS countries, "+
-//        "string_agg(DISTINCT sit.state, ', ' ORDER BY sit.state) AS areas, "+
-//        "string_agg(DISTINCT sit.site_code, ', ' ORDER BY sit.site_code) AS siteCodes, " +
-//        "string_agg(DISTINCT meo.ecoregion, ', ' ORDER BY meo.ecoregion) AS ecoRegions "+
-//        "FROM nrmn.location_ref loc " +
-//        "LEFT JOIN nrmn.site_ref sit ON loc.location_id = sit.location_id " +
-//        "LEFT JOIN nrmn.meow_ecoregions meo ON st_contains(meo.geom, sit.geom) " +
-//        "LEFT JOIN nrmn.survey sur ON sur.site_id = sit.site_id " +
-//        "GROUP BY loc.location_id, locationName", nativeQuery = true)
-//    List<LocationExtendedMapping> getAllWithRegions();
 }
