cbfd76b7dbdd7841a16d6e78623081a7e6660f26
utas-raymondng <manfai.ng@utas.edu.au>
2022-08-31 11:05:16 +1000

    Filter and sort of diver list done

---
+++ api/src/main/java/au/org/aodn/nrmn/restapi/model/db/DiverListView.java
@@ -0,0 +1,53 @@
+package au.org.aodn.nrmn.restapi.model.db;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import static org.hibernate.envers.RelationTargetAuditMode.NOT_AUDITED;
+
+/**
+ * This view is use by the GUI diver screen, we do not use the Diver db entity directly because
+ * we want to allow flexible table join here if needed in future
+ */
+@Entity
+@Immutable
+@Audited(withModifiedFlag = false)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Subselect(
+        "SELECT d.diver_id, d.initials, d.full_name FROM nrmn.diver_ref d"
+)
+public class DiverListView {
+    @Id
+    @Column(name="diver_id")
+    private int diverId;
+
+    @Column(name="initials")
+    @Audited(targetAuditMode = NOT_AUDITED)
+    private String initials;
+
+    @Column(name="full_name")
+    @Audited(targetAuditMode = NOT_AUDITED)
+    private String fullName;
+
+    @JsonGetter
+    public int getDiverId() {
+        return diverId;
+    }
+
+    @JsonGetter
+    public String getInitials() {
+        return initials;
+    }
+
+    @JsonGetter
+    public String getFullName() {
+        return fullName;
+    }
+}
