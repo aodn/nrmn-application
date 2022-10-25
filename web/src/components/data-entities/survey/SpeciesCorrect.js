81dcd699b939aac0bdf7afdd1ae4dcfd5125ca0d
nspool <25042597+nspool@users.noreply.github.com>
2022-10-25 15:42:19 +1100

    species correct: details panel wip

--- web/src/components/data-entities/survey/SpeciesCorrect.js
+++ web/src/components/data-entities/survey/SpeciesCorrect.js
@@ -1,36 +1,38 @@
 import {Box, Chip, LinearProgress, Typography} from '@mui/material';
-import React, {useEffect, useReducer} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
 import {getSurveySpecies} from '../../../api/api';
 import SpeciesCorrectFilter from './SpeciesCorrectFilter';
 import {PropTypes} from 'prop-types';
 
-const SpeciesCorrectResults = (results) => {
+const SpeciesCorrectResults = ({results, onClick}) => {
   return (
     <Box>
-      {results.results.map((r) => (
+      {results.map((r) => (
         <Box key={`${r.observableItemId}-${r.surveyId}`} display="flex" flexDirection="row">
           <div width="150px">{r.locationName}</div>
           <div width="100px">{r.surveyId}</div>
           <div width="100px">{r.surveyDate}</div>
           <div width="100px">{r.observableItemName}</div>
           <div width="200px">{r.commonName}</div>
+          <Chip label="View" onClick={() => onClick(r)} />
         </Box>
       ))}
     </Box>
   );
 };
 
-SpeciesCorrectResults.propsTypes = {
-  results: PropTypes.object
+SpeciesCorrectResults.propTypes = {
+  results: PropTypes.array,
+  onClick: PropTypes.func
 };
 
 const SpeciesCorrect = () => {
-  const [request, setRequest] = useReducer((state, action) => {
+  const [request, dispatch] = useReducer((state, action) => {
     switch (action.type) {
       case 'getRequest':
-        return {loading: true, results: null, request: action.payload};
+        return {loading: true, results: null, detail: null, request: action.payload};
       case 'showResults':
-        return {loading: false, request: null, results: action.payload};
+        return {loading: false, request: null, detail: null, results: action.payload};
       default:
         return state;
     }
@@ -39,24 +41,34 @@ const SpeciesCorrect = () => {
   useEffect(() => {
     const fetchSurveySpecies = async () => {
       const result = await getSurveySpecies(request.request);
-      setRequest({type: 'showResults', payload: result.data});
+      dispatch({type: 'showResults', payload: result.data});
     };
     if (request.request) fetchSurveySpecies();
   }, [request.request]);
+
+  const [detail, setDetail] = useState(null);
 
   return (
     <>
       <Box p={1}>
         <Typography variant="h4">Correct Species</Typography>
       </Box>
-      <SpeciesCorrectFilter onSearch={(filter) => setRequest({type: 'getRequest', payload: filter})} />
+      <SpeciesCorrectFilter onSearch={(filter) => dispatch({type: 'getRequest', payload: filter})} />
       {request.loading && <LinearProgress />}
       {request.results && (
         <Box display="flex" flex={2} overflow="hidden" flexDirection="row">
           <Box width="50%" overflow="scroll">
-            <SpeciesCorrectResults results={request.results} />
+            <SpeciesCorrectResults results={request.results} onClick={(r) => setDetail(r)} />
           </Box>
-          <Box>Thing</Box>
+          {detail && (
+            <Box>
+              <div width="150px">{detail.locationName}</div>
+              <div width="100px">{detail.surveyId}</div>
+              <div width="100px">{detail.surveyDate}</div>
+              <div width="100px">{detail.observableItemName}</div>
+              <div width="200px">{detail.commonName}</div>
+            </Box>
+          )}
         </Box>
       )}
     </>
