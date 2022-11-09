b99147e35ed83164f5ec5415374ecaed568b9729
nspool <25042597+nspool@users.noreply.github.com>
2022-11-09 12:49:26 +1100

    api: remove unused import

--- api/src/test/java/au/org/aodn/nrmn/restapi/controller/CorrectionsControllerIT.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/controller/CorrectionsControllerIT.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
