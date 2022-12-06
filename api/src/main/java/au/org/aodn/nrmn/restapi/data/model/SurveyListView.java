9bac6a924ef88cad3281f34fa1c466b4337547b2
utas-raymondng <manfai.ng@utas.edu.au>
2022-12-06 14:19:22 +1100

    change view to make it run faster

--- api/src/main/java/au/org/aodn/nrmn/restapi/data/model/SurveyListView.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/data/model/SurveyListView.java
@@ -7,15 +7,15 @@ import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.Subselect;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import static org.hibernate.envers.RelationTargetAuditMode.NOT_AUDITED;
 
 /**
  * This view is use by the GUI site screen, we do not use the survey db entity directly because
- * we want to allow flexible table join here if needed in future
+ * we want to allow flexible table join here if needed in the future. The view use another view which is
+ * costly and therefore it is expected that this view is using with Pageable. Otherwise, it will take forever to load
+ * the whole dataset.
  */
 @Entity
 @Immutable
@@ -25,22 +25,14 @@ import static org.hibernate.envers.RelationTargetAuditMode.NOT_AUDITED;
         "SELECT s.survey_id, s.locked, sr.site_code, to_char(s.survey_date, 'yyyy-MM-dd') as survey_date, " +
                 "       (s.depth || '.' || s.survey_num) as depth, sr.site_name, pr.program_name, " +
                 "       lr.location_name, CAST((s.pq_catalogued IS TRUE) as varchar) as pq_catalogued, COALESCE(sr.mpa, '') as mpa, " +
-                "       d.diver as full_name, d.method, meo.ecoregion, sr.country, sr.state, " +
+                "       meo.ecoregion, sr.country, sr.state, " +
                 "       ROUND(sr.latitude::numeric, " + Iirc.ROUNDING_DIGIT + ") as latitude, " +
                 "       ROUND(sr.longitude::numeric, " + Iirc.ROUNDING_DIGIT + ") as longitude " +
                 "FROM nrmn.survey s " +
                 "INNER JOIN nrmn.site_ref sr ON s.site_id = sr.site_id " +
                 "INNER JOIN nrmn.program_ref pr ON s.program_id = pr.program_id " +
                 "INNER JOIN nrmn.location_ref lr ON sr.location_id = lr.location_id " +
-                "LEFT JOIN nrmn.meow_ecoregions meo ON st_contains(meo.geom, sr.geom) " +
-                "LEFT OUTER JOIN ( " +  // Allow missing diver with outer join
-                "    SELECT sm.survey_id, " +
-                "           string_agg(DISTINCT sm.method_id::text,', ' ORDER BY sm.method_id::text ASC) as method, " +
-                "           string_agg(DISTINCT dr.full_name, ', ' ORDER BY dr.full_name ASC) as diver " +
-                "    FROM nrmn.observation o " +
-                "        INNER JOIN nrmn.survey_method sm on o.survey_method_id = sm.survey_method_id " +
-                "        INNER JOIN nrmn.diver_ref dr on dr.diver_id = o.diver_id " +
-                "    GROUP BY sm.survey_id) d on d.survey_id = s.survey_id"
+                "LEFT JOIN nrmn.meow_ecoregions meo ON st_contains(meo.geom, sr.geom) "
 )
 public class SurveyListView {
     @Id
@@ -88,14 +80,6 @@ public class SurveyListView {
     @Audited(targetAuditMode = NOT_AUDITED)
     private String state;
 
-    @Column(name = "full_name")
-    @Audited(targetAuditMode = NOT_AUDITED)
-    private String fullName;
-
-    @Column(name = "method")
-    @Audited(targetAuditMode = NOT_AUDITED)
-    private String method;
-
     @Column(name = "latitude")
     @Audited(targetAuditMode = NOT_AUDITED)
     private Double latitude;
@@ -107,6 +91,13 @@ public class SurveyListView {
     @Column(name = "ecoregion")
     @Audited(targetAuditMode = NOT_AUDITED)
     private String ecoregion;
+    /**
+     * This view is a heavy load view, hence we use lazy to load value if needed.
+     */
+    @Audited(targetAuditMode = NOT_AUDITED)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_id", referencedColumnName = "survey_id", nullable = false)
+    private DiverMethodAggregateView aggregateView;
 
     @JsonGetter
     public Integer getSurveyId() {
@@ -165,16 +156,6 @@ public class SurveyListView {
     }
 
     @JsonGetter
-    public String getDiverName() {
-        return fullName;
-    }
-
-    @JsonGetter
-    public String getMethod() {
-        return method;
-    }
-
-    @JsonGetter
     public String getEcoregion() {
         return ecoregion;
     }
@@ -188,5 +169,20 @@ public class SurveyListView {
     public String getLongitude() {
         return String.format(Iirc.FORMAT_DIGIT, longitude);
     }
-
+    /**
+     * Flatten the json to avoid nested structure
+     * @return
+     */
+    @JsonGetter
+    public String getDiverName() {
+        return aggregateView.getDiverName();
+    }
+    /**
+     * Flatten the json to avoid nested structure
+     * @return
+     */
+    @JsonGetter
+    public String getMethod() {
+        return aggregateView.getMethod();
+    }
 }
--- api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/SurveyListRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/SurveyListRepository.java
@@ -9,19 +9,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.QueryHints;
-
-import javax.persistence.QueryHint;
 
 import java.util.List;
 
-import static org.hibernate.jpa.QueryHints.HINT_CACHEABLE;
-
 public interface SurveyListRepository extends JpaRepository<SurveyListView, Integer>, JpaSpecificationExecutor<SurveyListView> {
 
-    @QueryHints({@QueryHint(name = HINT_CACHEABLE, value = "true")})
     default Page<SurveyListView> findAllProjectedBy(List<Filter> filters, List<Sorter> sort, Pageable pageable) {
-
         Specification<SurveyListView> spec = SurveyFilterCondition.createSpecification(filters, sort);
 
         return this.findAll(spec, pageable);
