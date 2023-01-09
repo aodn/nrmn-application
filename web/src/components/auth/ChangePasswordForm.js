c1f647d7025e505c78ecec74e7b5ec4c75ce019e
nspool <25042597+nspool@users.noreply.github.com>
2023-01-09 12:47:28 +1100

    web: password reset workflow

--- web/src/components/auth/ChangePasswordForm.js
+++ web/src/components/auth/ChangePasswordForm.js
@@ -1,41 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import Grid from '@mui/material/Grid';
 import LoadingButton from '@mui/lab/LoadingButton';
 import Typography from '@mui/material/Typography';
-import {PropTypes} from 'prop-types';
-import {Paper, TextField} from '@mui/material';
-import {changePassword} from '../../api/api';
-import {AuthContext} from '../../contexts/auth-context';
-import Chip from '@mui/material/Chip';
+import { PropTypes } from 'prop-types';
+import { Paper, TextField } from '@mui/material';
+import { changePassword } from '../../api/api';
+import { AuthContext } from '../../contexts/auth-context';
+import { useNavigate } from 'react-router-dom';
 
 const ChangePasswordForm = () => {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const version = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION.split('.') : [0, 0, 0];
+  const [error, setError] = useState(null);
 
   const onSubmit = (event, setAuth) => {
     event.preventDefault();
+    const { username, password, newPassword, confirmNewPassword } = event.target.elements;
+    if (newPassword.value !== confirmNewPassword.value) {
+      setError('New password fields do not match');
+      return;
+    }
     setLoading(true);
-    const {username, password, newPassword} = event.target.elements;
-    const form = {username: username?.value, password: password?.value, newPassword: newPassword?.value};
-    changePassword(form, (result, error) => {
+    const form = { username: username?.value, password: password?.value, newPassword: newPassword?.value };
+    changePassword(form, ({ data }) => {
       setLoading(false);
-      if (!error) setAuth(result);
+      if (data) {
+        setError(data);
+      } else {
+        localStorage.removeItem('auth');
+        setAuth({ expires: 0 });
+        navigate('/login?reset', { push: true });
+      }
     });
   };
 
   return (
     <AuthContext.Consumer>
-      {({setAuth}) => (
-        <Grid container alignItems="center" direction="column"  justifyContent="center" style={{minHeight: '70vh'}}>
+      {({ setAuth }) => (
+        <Grid container alignItems="center" direction="column" justifyContent="center" style={{ minHeight: '70vh' }}>
           <Paper>
-            <Box paddingX={20} paddingY={5}>
+            <Box paddingX={20} paddingY={5} minWidth="400px">
               <Typography variant="h4">Change Password</Typography>
               <hr />
-              <Alert data-testid="alert" severity="warning">
-                Please update your password
-              </Alert>
+              {error ? (<Alert data-testid="alert" severity="error">
+                {error}
+              </Alert>) : (<Alert data-testid="alert" severity="warning">
+                Please choose a new password at least 8 characters long
+              </Alert>)}
               <form role="form" onSubmit={(e) => onSubmit(e, setAuth)}>
                 <Box my={2}>
                   <Typography variant="subtitle2">Email</Typography>
@@ -61,13 +74,23 @@ const ChangePasswordForm = () => {
                     disabled={loading}
                   />
                 </Box>
-                 <Box my={2}>
+                <Box my={2}>
                   <Typography variant="subtitle2">New Password</Typography>
                   <TextField
                     size="small"
                     name="newPassword"
                     type="password"
-                    placeholder="New Password"
+                    fullWidth
+                    color="primary"
+                    disabled={loading}
+                  />
+                </Box>
+                <Box my={2}>
+                  <Typography variant="subtitle2">Confirm New Password</Typography>
+                  <TextField
+                    size="small"
+                    name="confirmNewPassword"
+                    type="password"
                     fullWidth
                     color="primary"
                     disabled={loading}
@@ -81,9 +104,6 @@ const ChangePasswordForm = () => {
               </form>
             </Box>
           </Paper>
-          <Box m={2}>
-            <Chip variant="outlined" color="primary" size="small" label={`Version ${version[0]}.${version[1]} (${version[2]})`}/>
-          </Box>
         </Grid>
       )}
     </AuthContext.Consumer>
--- web/src/components/auth/LoginForm.js
+++ web/src/components/auth/LoginForm.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
 import Grid from '@mui/material/Grid';
 import LoadingButton from '@mui/lab/LoadingButton';
 import Typography from '@mui/material/Typography';
-import {PropTypes} from 'prop-types';
-import {Paper, TextField} from '@mui/material';
-import {userLogin} from '../../api/api';
-import {AuthContext} from '../../contexts/auth-context';
+import { PropTypes } from 'prop-types';
+import { Paper, TextField } from '@mui/material';
+import { userLogin } from '../../api/api';
+import { AuthContext } from '../../contexts/auth-context';
 import Chip from '@mui/material/Chip';
 
 const LoginForm = () => {
@@ -18,26 +18,32 @@ const LoginForm = () => {
   const onSubmit = (event, setAuth) => {
     event.preventDefault();
     setLoading(true);
-    const {username, password} = event.target.elements;
-    const form = {username: username?.value, password: password?.value};
+    const { username, password } = event.target.elements;
+    const form = { username: username?.value, password: password?.value };
     userLogin(form, (result, error) => {
-      setFormState(error ? {error} : {result});
+      setFormState(error ? { error } : { result });
       setLoading(false);
       if (!error) setAuth(result);
     });
   };
 
+  const wasPasswordReset = window.location.search.includes('reset');
+
   return (
     <AuthContext.Consumer>
-      {({setAuth}) => (
-        <Grid container alignItems="center" direction="column"  justifyContent="center" style={{minHeight: '70vh'}}>
+      {({ setAuth }) => (
+        <Grid container alignItems="center" direction="column" justifyContent="center" style={{ minHeight: '70vh' }}>
           <Paper>
-            <Box paddingX={20} paddingY={5}>
+            <Box paddingX={20} paddingY={5} minWidth="400px">
               <Typography variant="h4">Login</Typography>
               <hr />
-              <Alert data-testid="alert" severity={formState?.error ? 'error' : 'info'}>
-                {formState?.error ? formState.error : formState?.result ? formState.result.username : 'Please login in to view this page'}
-              </Alert>
+              {wasPasswordReset && !formState?.error ? (<Alert data-testid="alert" severity="success">
+                Password reset successfully!<br />
+                Please log in with your new password
+              </Alert>) :
+                (<Alert data-testid="alert" severity={formState?.error ? 'error' : 'info'}>
+                  {formState?.error ? formState.error : formState?.result ? formState.result.username : 'Please login in to view this page'}
+                </Alert>)}
               <form role="form" onSubmit={(e) => onSubmit(e, setAuth)}>
                 <Box my={2}>
                   <Typography variant="subtitle2">Email</Typography>
@@ -72,7 +78,7 @@ const LoginForm = () => {
             </Box>
           </Paper>
           <Box m={2}>
-            <Chip variant="outlined" color="primary" size="small" label={`Version ${version[0]}.${version[1]} (${version[2]})`}/>
+            <Chip variant="outlined" color="primary" size="small" label={`Version ${version[0]}.${version[1]} (${version[2]})`} />
           </Box>
         </Grid>
       )}
