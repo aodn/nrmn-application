bdaba29eec05030fa1fd88fa9f8672612aca4260
nspool <25042597+nspool@users.noreply.github.com>
2023-01-30 15:30:04 +1100

    meow validation: rewrite query
    instead of using the endpoint, query directly giving an ecoregion
    and list of species.

--- api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/MeowRegionsRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/MeowRegionsRepository.java
@@ -9,11 +9,16 @@ import org.springframework.data.repository.query.Param;
 
 import au.org.aodn.nrmn.restapi.data.model.MeowEcoRegions;
 
-public interface MeowRegionsRepository extends JpaRepository<MeowEcoRegions, Integer>, JpaSpecificationExecutor<MeowEcoRegions> {
-    
-    @Query(value = "WITH stp1 AS(" +
-        "SELECT DISTINCT(ecoregion)AS region, species_name FROM {h-schema}ep_m2_cryptic_fish GROUP BY species_name,ecoregion) " +
-        "SELECT DISTINCT species_name FROM stp1 WHERE species_name in (:species_names) AND region=:eco_region " +
-        "", nativeQuery = true)
+public interface MeowRegionsRepository
+        extends JpaRepository<MeowEcoRegions, Integer>, JpaSpecificationExecutor<MeowEcoRegions> {
+
+    @Query(value = "select ecoregion, si.site_id " +
+    "from {h-schema}observable_item_ref oi " +
+    "inner join {h-schema}observation o ON o.observable_item_id = oi.observable_item_id " +
+    "inner join {h-schema}survey_method sm ON o.survey_method_id = sm.survey_method_id " +
+    "inner join {h-schema}survey s ON sm.survey_id = s.survey_id " +
+    "inner join {h-schema}site_ref si ON s.site_id = si.site_id " +
+    "inner join {h-schema}meow_ecoregions m ON st_contains(m.geom, si.geom) " +
+    "where observable_item_name in (:species_names) AND ecoregion = :eco_region", nativeQuery = true)
     Collection<String> getEcoregionContains(@Param("eco_region") String ecoRegion, @Param("species_names") Collection<String> speciesNames);
 }
