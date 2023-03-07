122d132e8ec5b291a3923cd33076f4f19f3fa12e
nspool <25042597+nspool@users.noreply.github.com>
2023-03-07 16:18:26 +1100

    db: extend staged_job_log
    add survey_id and summary for correction summary.

---
+++ db/schema-update/01-add-staged-log-surveyid-summary.sql
@@ -0,0 +1,9 @@
+BEGIN TRANSACTION;
+
+ALTER TABLE nrmn.staged_job_log DROP COLUMN IF EXISTS survey_id;
+ALTER TABLE nrmn.staged_job_log DROP COLUMN IF EXISTS summary;
+
+ALTER TABLE nrmn.staged_job_log ADD COLUMN survey_id INT;
+ALTER TABLE nrmn.staged_job_log ADD COLUMN summary JSON;
+
+END TRANSACTION
