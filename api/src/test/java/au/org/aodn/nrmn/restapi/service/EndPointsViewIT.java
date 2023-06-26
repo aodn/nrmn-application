b4e2a829a8be7c370b1a30cd39459112e5ebb08a
utas-raymondng <manfai.ng@utas.edu.au>
2023-06-26 11:03:29 +1000

    Add sample run

--- api/src/test/java/au/org/aodn/nrmn/restapi/service/EndPointsViewIT.java
+++ api/src/test/java/au/org/aodn/nrmn/restapi/service/EndPointsViewIT.java
@@ -5,24 +5,76 @@ import au.org.aodn.nrmn.restapi.test.annotations.WithTestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
+import java.sql.ResultSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
 
 @Testcontainers
 @SpringBootTest
 @WithTestData
+@Transactional
 @ExtendWith(PostgresqlContainerExtension.class)
 public class EndPointsViewIT {
 
+    final static Logger logger = LoggerFactory.getLogger(EndPointsViewIT.class);
+
+    class EpObservableItems {
+        Integer rowNum;
+        String name;
+        Integer id;
+    }
+
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @BeforeEach
     @Sql({"../../../db/endpoints/CeatePrivateEndpoints.sql", "/testdata/FILL_ENDPOINT_DATA.sql"})
-    public void injectTestData() {}
+    public void injectTestData() {
+        Arrays.asList(
+                "ep_rarity_extents","ep_rarity_range","ep_rarity_abundance","ep_rarity_frequency","ep_observable_items")
+                .forEach(i -> {
+                    logger.info("Populate materialized view " + i);
+                    jdbcTemplate.execute("REFRESH MATERIALIZED VIEW nrmn." + i);
+                });
+    }
 
     /**
      * Test on ep_observable_items view
      */
     @Test
-    public void verifyEPSpeciesLIst() {}
+    public void verifyEPSpeciesLIst() {
+        List<EpObservableItems> objs = jdbcTemplate.query("select * from nrmn.ep_observable_items", (ResultSet rs, int rowNum) -> {
+            EpObservableItems i = new EpObservableItems();
+
+            i.rowNum = rowNum;
+            i.id = rs.getInt(1);
+            i.name = rs.getString(2);
+
+            return i;
+        });
+
+        assertEquals("Total row count for ep_observable_items", 5, objs.size());
+
+        AtomicInteger j = new AtomicInteger(0);
+        Arrays.asList("Debris","Duplicate rubra", "Haliotis rubra", "Species 56","Species 57")
+                .forEach(i -> assertEquals("Species name match", i, objs.get(j.getAndIncrement()).name));
+    }
 
 }
