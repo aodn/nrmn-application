ec3f7a626efc6176f4b9148b8ab5b5be758739bd
Havier <huaizhi.dai@utas.edu.au>
2023-11-23 11:12:59 +1100

    A new component BackButton for the reference of all go back action.

--- web/src/components/containers/EntityContainer.js
+++ web/src/components/containers/EntityContainer.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import {Box, Grid, Typography} from '@mui/material';
-import {NavLink} from 'react-router-dom';
+import {Box, Grid} from '@mui/material';
 import {PropTypes} from 'prop-types';
+import BackButton from '../ui/BackButton';
 
 const EntityContainer = (props) => (
   <>
     <Box m={1}>
-      <NavLink to={props.goBackTo}>
-        <Typography
-          variant="button"
-          sx={{textTransform: 'none', fontSize: '1rem', fontWeight: 400 }}
-        >
-          {'<< Back to ' + props.name}
-        </Typography>
-      </NavLink>
+      <BackButton goBackTo={props.goBackTo} name={props.name} />
     </Box>
     <Grid container justifyContent="center">
       {props.header}
