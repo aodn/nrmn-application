f60f8f4c2416ae4339af95d105062dae97589459
bpasquer <benedicte.pasquer@utas.edu.au>
2024-01-03 14:42:47 +1100

    Select species by current name and total abundance

--- api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/ObservableItemRepository.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/data/repository/ObservableItemRepository.java
@@ -53,17 +53,19 @@ public interface ObservableItemRepository extends JpaRepository<ObservableItem, 
 //    @Query("SELECT o from ObservableItem o WHERE o.observableItemName = :name")
 //    ObservableItem getWithName(@Param("name") String name);
 
-    @Query(value = "select distinct on (obsitem.observable_item_id) obsitem.observable_item_id as observableItemId, obsitem.observable_item_name as name, obsitem.letter_code as letterCode, obsitem.common_name as commonName "
+    @Query(value = "WITH stage0 AS(SELECT COALESCE(obsitem.superseded_by, obsitem.observable_item_name) AS name, sum(obs.measure_value) AS abundance "
             + "FROM {h-schema}location_ref loc "
             + "INNER JOIN {h-schema}site_ref site_raw ON site_raw.location_id = loc.location_id "
-            + "INNER JOIN {h-schema}ep_site_list site ON site.site_code = site_raw.site_code "
             + "INNER JOIN {h-schema}survey sur ON sur.site_id = site_raw.site_id "
             + "INNER JOIN {h-schema}survey_method surmet ON surmet.survey_id = sur.survey_id "
             + "INNER JOIN {h-schema}observation obs ON obs.survey_method_id = surmet.survey_method_id "
             + "INNER JOIN {h-schema}observable_item_ref obsitem ON obsitem.observable_item_id = obs.observable_item_id "
             + "INNER JOIN {h-schema}methods_species m ON m.observable_item_id = obsitem.observable_item_id "
-            + "where m.method_id = :methodId AND site_raw.site_id IN :siteIds AND obsitem.superseded_by IS NULL "
-            + "AND (:methodId != 2 OR (obsitem.class NOT IN ('Ophiuroidea', 'Polyplacophora') AND obsitem.family != 'Pyuridae'))", nativeQuery = true)
+            + "WHERE m.method_id = :methodId AND site_raw.site_id IN :siteIds "
+            + "AND (:methodId != 2 OR (obsitem.class NOT IN ('Ophiuroidea', 'Polyplacophora') AND obsitem.family != 'Pyuridae')) GROUP BY name) "
+            + "SELECT oir.observable_item_id AS observableItemId, name, oir.letter_code AS letterCode, oir.common_name AS commonName "
+            + "FROM stage0 "
+            + "INNER JOIN {h-schema}observable_item_ref oir on oir.observable_item_name=stage0.name", nativeQuery = true)
     List<ObservableItemRow> getAllWithMethodForSites(@Param("methodId") Integer methodId,
             @Param("siteIds") Collection<Integer> siteIds);
 
