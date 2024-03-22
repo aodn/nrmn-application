8acf0355fa16d325724af5f40b0e4fc3f3048fd5
Havier Dai <huaizhi.dai@utas.edu.au>
2024-03-22 16:27:33 +1100

    libraries upgrading.

--- web/src/components/data-entities/survey/__test__/SpeciesCorrect.test.tsx
+++ web/src/components/data-entities/survey/__test__/SpeciesCorrect.test.tsx
@@ -1,6 +1,5 @@
 // @ts-ignore
 import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
 import { render, waitFor, screen, fireEvent, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
