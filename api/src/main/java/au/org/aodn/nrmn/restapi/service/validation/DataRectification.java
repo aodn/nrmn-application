66153c8eee9f71e878cc662f90081fa838167a71
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-05-28 16:35:53 +1000

    extract multi-used message

--- api/src/main/java/au/org/aodn/nrmn/restapi/service/validation/DataRectification.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/service/validation/DataRectification.java
@@ -5,10 +5,9 @@ import au.org.aodn.nrmn.restapi.data.model.StagedRow;
 import au.org.aodn.nrmn.restapi.dto.stage.SurveyValidationError;
 import org.apache.commons.math3.util.Precision;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
-import static au.org.aodn.nrmn.restapi.util.Constants.COORDINATE_VALID_DECIMAL_COUNT;
+import static au.org.aodn.nrmn.restapi.util.Constants.*;
 
 /**
  * This class is used to automatically rectify the data during the validation process.
@@ -27,56 +26,53 @@ public class DataRectification {
 
     public Collection<StagedRow> rectify() {
         // If new data need to be rectified by the system, please add new functions and run them here.
-        rectifyValidatedLatLonInDiff();
-        rectifyOthers();
+        for (var error: errors) {
+            rectifyValidatedLatLonInDiff(error);
+            rectifyOthers(error);
+        }
 
         return rows;
     }
 
-    private void rectifyValidatedLatLonInDiff() {
-        var resultRows = new ArrayList<>(rows);
-        for (var error : errors) {
-            if (error.getMessage().contains("This row will use the site's coordinates.")) {
-                var rowId = error.getRowIds().iterator().next();
-                var row = resultRows.stream().filter(r -> r.getId().equals(rowId)).findFirst().orElse(null);
-                if (row != null) {
-                    row.setLatitude(null);
-                    row.setLongitude(null);
-                }
-                continue;
+    private void rectifyValidatedLatLonInDiff(SurveyValidationError error) {
+        if (error.getMessage().contains(NULLIFY_LAT_LON_MSG_SUFFIX)) {
+            var rowId = error.getRowIds().iterator().next();
+            var row = rows.stream().filter(r -> r.getId().equals(rowId)).findFirst().orElse(null);
+            if (row != null) {
+                row.setLatitude(null);
+                row.setLongitude(null);
             }
-            if (error.getMessage().contains("Longitude will be rounded to 5 decimal places")) {
-                var rowId = error.getRowIds().iterator().next();
-                resultRows
-                        .stream()
-                        .filter(r -> r.getId() != null && r.getId().equals(rowId))
-                        .findFirst()
-                        .ifPresent(row ->
-                                row.setLongitude(String.valueOf(
-                                        Precision.round(
-                                                Double.parseDouble(row.getLongitude()),
-                                                COORDINATE_VALID_DECIMAL_COUNT
-                                        ))));
-                continue;
-            }
-            if (error.getMessage().contains("Latitude will be rounded to 5 decimal places")) {
-                var rowId = error.getRowIds().iterator().next();
-                resultRows
-                        .stream()
-                        .filter(r -> r.getId() != null && r.getId().equals(rowId))
-                        .findFirst()
-                        .ifPresent(row ->
-                                row.setLatitude(String.valueOf(
-                                        Precision.round(
-                                                Double.parseDouble(row.getLatitude()), COORDINATE_VALID_DECIMAL_COUNT
-                                        ))));
-            }
+            return;
         }
-        rows = resultRows;
+        if (error.getMessage().contains(ROUND_LON_MSG)) {
+            var row = findRelatedRowOf(error);
+            if (row != null) {
+                row.setLongitude(String.valueOf(
+                        Precision.round(
+                                Double.parseDouble(row.getLongitude()), COORDINATE_VALID_DECIMAL_COUNT
+                        )));
+            }
+            return;
+        }
+        if (error.getMessage().contains(ROUND_LAT_MSG)) {
+
+                var row = findRelatedRowOf(error);
+                if (row != null) {
+                    row.setLatitude(String.valueOf(
+                            Precision.round(
+                                    Double.parseDouble(row.getLatitude()), COORDINATE_VALID_DECIMAL_COUNT
+                            )));
+                }
+        }
+    }
+
+    private StagedRow findRelatedRowOf(SurveyValidationError error) {
+        var rowId = error.getRowIds().iterator().next();
+        return rows.stream().filter(r -> r.getId().equals(rowId)).findFirst().orElse(null);
     }
 
     /**
      * This method is used to show the pattern of how to use new rectification methods in this class.
      */
-    private void rectifyOthers() {}
+    private void rectifyOthers(SurveyValidationError ignored) {}
 }
--- api/src/main/java/au/org/aodn/nrmn/restapi/util/Constants.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/util/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
 
     // The number of decimal places to use when validating coordinates
     public static final int COORDINATE_VALID_DECIMAL_COUNT = 5;
+
+    public static final String ROUND_LON_MSG = "Longitude will be rounded to 5 decimal places";
+    public static final String ROUND_LAT_MSG = "Latitude will be rounded to 5 decimal places";
+    public static final String NULLIFY_LAT_LON_MSG_SUFFIX = "This row will use the site's coordinates.";
 }
