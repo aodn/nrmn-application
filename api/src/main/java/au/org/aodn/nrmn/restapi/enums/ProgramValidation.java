422a898f045f6d0aa5dffd0019c0e705b126e027
HuaizhiDai <huaizhi.dai@utas.edu.au>
2026-01-16 14:32:32 +1100

    add RRH into program validation

--- api/src/main/java/au/org/aodn/nrmn/restapi/enums/ProgramValidation.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/enums/ProgramValidation.java
@@ -8,7 +8,11 @@ import au.org.aodn.nrmn.restapi.data.model.Program;
 public enum ProgramValidation {
     NONE(null),
     RLS(LocalDate.parse("2006-01-01")),
-    ATRC(LocalDate.parse("1991-01-01"));
+    ATRC(LocalDate.parse("1991-01-01")),
+
+    // TODO: confirm date
+    RRH(LocalDate.parse("1991-01-01"))
+    ;
 
     LocalDate minDate;
 
@@ -18,9 +22,16 @@ public enum ProgramValidation {
 
     public static ProgramValidation fromProgram(Program program) {
         var programName = program.getProgramName().toUpperCase();
-        return Arrays.asList("ATRC", "FRDC").contains(programName) ? ProgramValidation.ATRC
-        : Arrays.asList("RLS", "PARKS VIC").contains(programName) ? ProgramValidation.RLS
-                : ProgramValidation.NONE;
+        if (Arrays.asList("ATRC", "FRDC").contains(programName)) {
+            return ProgramValidation.ATRC;
+        }
+        if (Arrays.asList("RLS", "PARKS VIC").contains(programName)) {
+            return ProgramValidation.RLS;
+        }
+        if (Arrays.asList("RRH").contains(programName)) {
+            return ProgramValidation.RRH;
+        }
+        return ProgramValidation.NONE;
     }
 
     public LocalDate getMinDate() {return minDate;}
